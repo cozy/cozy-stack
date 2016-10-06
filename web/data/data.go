2727854dd0c06fa38cd96b11384628f818bbba48
Romain <romain@cozycloud.cc>
2016-10-06 12:07:01 +0200

    use the improvement to error handling to simplify controllers

--- web/data/data.go
+++ web/data/data.go
@@ -2,83 +2,58 @@
 package data
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/cozy/cozy-stack/couchdb"
+	"github.com/cozy/cozy-stack/web/errors"
 	"github.com/cozy/cozy-stack/web/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
-func abortNoInstance(c *gin.Context) {
-	err := fmt.Errorf("No instance found")
-	c.AbortWithError(http.StatusInternalServerError, err)
-}
-
-func makeCode(err error) int {
-	coucherr, iscoucherr := err.(*couchdb.Error)
-	if iscoucherr {
-		return coucherr.StatusCode
-	}
-
-	return http.StatusInternalServerError
-}
-
-func validDoctype(doctype string) bool {
+func validDoctype(c *gin.Context) {
 	// TODO extends me to verificate characters allowed in db name.
-	return doctype != ""
+	doctype := c.Param("doctype")
+	if doctype == "" {
+		c.AbortWithError(http.StatusBadRequest, errors.InvalidDoctype(doctype))
+	} else {
+		c.Set("doctype", doctype)
+	}
 }
 
 // GetDoc get a doc by its type and id
-func GetDoc(c *gin.Context) {
-	// @TODO this should be extracted to a middleware
-	instance, exists := c.Get("instance")
-	if !exists {
-		abortNoInstance(c)
-		return
-	}
+func getDoc(c *gin.Context) {
+	instance := c.MustGet("instance").(*middlewares.Instance)
+	doctype := c.MustGet("doctype").(string)
 
-	prefix := instance.(*middlewares.Instance).GetDatabasePrefix()
+	prefix := instance.GetDatabasePrefix()
+
 	var out couchdb.Doc
-
-	reqerr := couchdb.GetDoc(prefix, c.Param("doctype"), c.Param("docid"), out)
-	if reqerr != nil {
-		coucherr, iscoucherr := reqerr.(*couchdb.Error)
-		if iscoucherr {
-			c.AbortWithError(coucherr.StatusCode, coucherr)
-		} else {
-			c.AbortWithError(http.StatusInternalServerError, reqerr)
-		}
+	err := couchdb.GetDoc(prefix, doctype, c.Param("docid"), &out)
+	if err != nil {
+		c.AbortWithError(errors.HTTPStatus(err), err)
 		return
 	}
 	c.JSON(200, out)
 }
 
 // CreateDoc create doc from the json passed as body
-func CreateDoc(c *gin.Context) {
+func createDoc(c *gin.Context) {
+	doctype := c.MustGet("doctype").(string)
 	instance := c.MustGet("instance").(*middlewares.Instance)
-	doctype := c.Param("doctype")
-	if !validDoctype(doctype) {
-		err := fmt.Errorf("Invalid document type.")
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	var doc couchdb.Doc
-	err := c.BindJSON(&doc)
-	if err != nil {
-		err := fmt.Errorf("invalid body")
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
 	prefix := instance.GetDatabasePrefix()
 
-	reqerr := couchdb.CreateDoc(prefix, c.Param("doctype"), doc)
-	if reqerr != nil {
-		c.AbortWithError(makeCode(reqerr), reqerr)
+	var doc couchdb.Doc
+	if err := c.BindJSON(&doc); err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
+
+	err := couchdb.CreateDoc(prefix, doctype, doc)
+	if err != nil {
+		c.AbortWithError(errors.HTTPStatus(err), err)
+		return
+	}
+
 	c.JSON(200, gin.H{
 		"ok":   true,
 		"id":   doc["_id"],
@@ -90,7 +65,7 @@ func CreateDoc(c *gin.Context) {
 
 // Routes sets the routing for the status service
 func Routes(router *gin.RouterGroup) {
-	router.GET("/:doctype/:docid", GetDoc)
-	router.POST("/:doctype", CreateDoc)
+	router.GET("/:doctype/:docid", validDoctype, getDoc)
+	router.POST("/:doctype", validDoctype, createDoc)
 	// router.DELETE("/:doctype/:docid", DeleteDoc)
 }
--- web/data/data_test.go
+++ web/data/data_test.go
@@ -3,33 +3,40 @@ package data
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/cozy/cozy-stack/web/errors"
 	"github.com/cozy/cozy-stack/web/middlewares"
 	"github.com/gin-gonic/gin"
+	"github.com/sourcegraph/checkup"
 	"github.com/stretchr/testify/assert"
 )
 
 var client = &http.Client{}
-var DOCUMENT = map[string]string{
-	"_id":  TYPE + "/" + ID,
-	"test": "testvalue",
-}
 
+const COUCHURL = "http://localhost:5984/"
 const HOST = "example.com"
 const TYPE = "io.cozy.events"
 const ID = "4521C325F6478E45"
 const EXPECTEDDBNAME = "example-com%2Fio-cozy-events"
 
+var DOCUMENT = []byte(`{
+	"test": "testvalue"
+}`)
+
+var ts *httptest.Server
+
 // @TODO this should be moved to our couchdb package or to
 // some test helpers files.
 
 func couchReq(method, path string, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(method, "http://localhost:5984/"+path, body)
+	req, err := http.NewRequest(method, COUCHURL+path, body)
 	if err != nil {
 		return nil, err
 	}
@@ -44,6 +51,7 @@ func couchReq(method, path string, body io.Reader) (*http.Response, error) {
 func testRoute(t *testing.T, url string, host string, jsonout interface{}) (
 	*http.Response, []byte, error) {
 
+	fmt.Println("test req", url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, nil, err
@@ -63,56 +71,72 @@ func testRoute(t *testing.T, url string, host string, jsonout interface{}) (
 
 }
 
-// prepareCouch destroy and re-create the database
-func prepareCouchdb(t *testing.T) {
-	_, err := couchReq("DELETE", EXPECTEDDBNAME, nil)
-	assert.NoError(t, err)
-
-	_, err = couchReq("PUT", EXPECTEDDBNAME, nil)
-	assert.NoError(t, err)
-
-	jsonbytes, _ := json.Marshal(DOCUMENT)
-	reqbody := bytes.NewReader(jsonbytes)
-	_, err = couchReq("PUT", EXPECTEDDBNAME+"/"+TYPE+"%2F"+ID, reqbody)
-	assert.NoError(t, err)
-}
-
 func injectInstance(instance *middlewares.Instance) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Set("instance", instance)
 	}
 }
 
-func TestRoutes(t *testing.T) {
+func TestMain(m *testing.M) {
+
+	// First we make sure couchdb is started
+	couchdb, err := checkup.HTTPChecker{URL: COUCHURL}.Check()
+	if err != nil || couchdb.Status() != checkup.Healthy {
+		fmt.Println("This test need couchdb to run.")
+		os.Exit(1)
+	}
+
 	router := gin.New()
 	instance := &middlewares.Instance{
 		Domain:     HOST,
 		StorageURL: "mem://test",
 	}
-	router.Use(gin.ErrorLogger())
+	router.Use(errors.Handler())
 	router.Use(injectInstance(instance))
 	Routes(router.Group("/data"))
-	ts := httptest.NewServer(router)
+	ts = httptest.NewServer(router)
+	couchReq("DELETE", EXPECTEDDBNAME, nil)
+	couchReq("PUT", EXPECTEDDBNAME, nil)
+	couchReq("PUT", EXPECTEDDBNAME+"/"+TYPE+"%2F"+ID, bytes.NewReader(DOCUMENT))
+
 	defer ts.Close()
+	os.Exit(m.Run())
+}
 
-	prepareCouchdb(t)
-
-	var out interface{}
+func TestSuccessGet(t *testing.T) {
+	var out map[string]interface{}
 	res, _, err := testRoute(t, ts.URL+"/data/"+TYPE+"/"+ID, HOST, &out)
 	assert.NoError(t, err)
 	assert.Equal(t, "200 OK", res.Status, "should get a 200")
-	jsonmap, ok := out.(map[string]interface{})
-	if assert.NotNil(t, ok, "should contains json") {
-		assert.Equal(t, jsonmap["test"], "testvalue", "should give the same doc")
+	if assert.Contains(t, out, "test") {
+		assert.Equal(t, out["test"], "testvalue", "should give the same doc")
+	}
+}
+
+func TestWrongDoctype(t *testing.T) {
+	var out map[string]interface{}
+	res, _, err := testRoute(t, ts.URL+"/data/nottype/"+ID, HOST, &out)
+	assert.NoError(t, err)
+	fmt.Println("RESULT=", out)
+	assert.Equal(t, "404 Not Found", res.Status, "should get a 404")
+	if assert.Contains(t, out, "error") {
+		assert.Equal(t, "not_found", out["error"], "should give a json error")
+	}
+	if assert.Contains(t, out, "reason") {
+		assert.Equal(t, "wrong_doctype", out["reason"], "should give a reason")
 	}
 
-	var out2 interface{}
-	res, _, err = testRoute(t, ts.URL+"/data/nottype/"+ID, HOST, &out2)
-	assert.NoError(t, err)
-	assert.Equal(t, "404 Not Found", res.Status, "should get a 404")
+}
 
-	var out3 interface{}
-	res, _, err = testRoute(t, ts.URL+"/data/"+TYPE+"/NOTID", HOST, &out3)
+func TestWrongID(t *testing.T) {
+	var out map[string]interface{}
+	res, _, err := testRoute(t, ts.URL+"/data/"+TYPE+"/NOTID", HOST, &out)
 	assert.NoError(t, err)
 	assert.Equal(t, "404 Not Found", res.Status, "should get a 404")
+	if assert.Contains(t, out, "error") {
+		assert.Equal(t, "not_found", out["error"], "should give a json error")
+	}
+	if assert.Contains(t, out, "reason") {
+		assert.Equal(t, "missing", out["reason"], "should give a reason")
+	}
 }
