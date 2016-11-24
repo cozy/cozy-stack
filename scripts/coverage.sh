c568af566b73da9476b19b3ac43b953b2616c2d2
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2016-11-24 12:24:52 +0100

    Space to Tabs in coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,9 +4,9 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=profile.out -covermode=count $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
+	go test -coverprofile=profile.out -covermode=count $d
+	if [ -f profile.out ]; then
+		cat profile.out >> coverage.txt
+		rm profile.out
+	fi
 done
