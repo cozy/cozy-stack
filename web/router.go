1873b79b4c82590763a0f1d1206474242fc138e7
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2016-11-25 09:24:39 +0100

    Add CORS headers

--- web/router.go
+++ web/router.go
@@ -23,6 +23,11 @@
 package web
 
 import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/cozy/cozy-stack/web/apps"
 	"github.com/cozy/cozy-stack/web/auth"
 	"github.com/cozy/cozy-stack/web/data"
@@ -38,10 +43,60 @@ func SetupRoutes(router *gin.Engine) {
 	router.Use(middlewares.ParseHost())
 	router.Use(middlewares.ServeApp(apps.Serve))
 	router.Use(middlewares.ErrorHandler())
+	router.Use(corsMiddleware())
+
 	auth.Routes(router)
 	apps.Routes(router.Group("/apps", middlewares.NeedInstance()))
 	data.Routes(router.Group("/data", middlewares.NeedInstance()))
 	files.Routes(router.Group("/files", middlewares.NeedInstance()))
 	status.Routes(router.Group("/status"))
 	version.Routes(router.Group("/version"))
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	normalHeaders := make(http.Header)
+	normalHeaders.Set("Access-Control-Allow-Credentials", "true")
+
+	preflightHeaders := make(http.Header)
+	preflightHeaders.Set("Access-Control-Allow-Credentials", "true")
+	preflightHeaders.Set("Access-Control-Allow-Methods", "*")
+	preflightHeaders.Set("Access-Control-Max-Age", strconv.FormatInt(int64(12*time.Hour/time.Second), 10))
+
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		if origin == "" {
+			return
+		}
+
+		path := c.Request.URL.Path
+		valid := (strings.HasPrefix(path, "/apps") ||
+			strings.HasPrefix(path, "/data") ||
+			strings.HasPrefix(path, "/files"))
+
+		if !valid {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		isOptions := c.Request.Method == "OPTIONS"
+
+		var headers http.Header
+		if isOptions {
+			headers = preflightHeaders
+		} else {
+			headers = normalHeaders
+		}
+
+		header := c.Writer.Header()
+		for key, value := range headers {
+			header[key] = value
+		}
+
+		c.Header("Access-Control-Allow-Origin", origin)
+
+		if isOptions {
+			c.Header("Access-Control-Allow-Headers", c.Request.Header.Get("Access-Control-Request-Headers"))
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+	}
 }
