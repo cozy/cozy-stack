db559a7d253b236fab2294833ab7a1925aadaa40
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2016-12-12 14:20:50 +0100

    Adapt tests to remove json serialization

--- crypto/mac_test.go
+++ crypto/mac_test.go
@@ -11,7 +11,7 @@ import (
 var testStrings = []string{"foo", "bar", "baz"}
 
 func TestMACMessageWithName(t *testing.T) {
-	value := "myvalue"
+	value := []byte("myvalue")
 
 	o1 := &MACConfig{
 		Key:  []byte("0123456789012345"),
@@ -30,27 +30,25 @@ func TestMACMessageWithName(t *testing.T) {
 	if !assert.NoError(t, err1) {
 		return
 	}
-	var dst string
-	err2 := DecodeAuthMessage(o1, encoded, &dst)
+	v, err2 := DecodeAuthMessage(o1, encoded)
 	if !assert.NoError(t, err2) {
 		return
 	}
-	if !assert.EqualValues(t, dst, value) {
-		t.Fatalf("Expected %v, got %v.", value, dst)
+	if !assert.EqualValues(t, v, value) {
+		t.Fatalf("Expected %v, got %v.", v, value)
 	}
-	var dst2 string
-	err3 := DecodeAuthMessage(o2, encoded, &dst2)
+	_, err3 := DecodeAuthMessage(o2, encoded)
 	if !assert.Error(t, err3) {
 		return
 	}
-	err4 := DecodeAuthMessage(o3, encoded, &dst2)
+	_, err4 := DecodeAuthMessage(o3, encoded)
 	if !assert.Error(t, err4) {
 		return
 	}
 }
 
 func TestMACMessageWithoutName(t *testing.T) {
-	value := "myvalue"
+	value := []byte("myvalue")
 
 	o1 := &MACConfig{
 		Key:  []byte("0123456789012345"),
@@ -69,20 +67,18 @@ func TestMACMessageWithoutName(t *testing.T) {
 	if !assert.NoError(t, err1) {
 		return
 	}
-	var dst string
-	err2 := DecodeAuthMessage(o1, encoded, &dst)
+	v, err2 := DecodeAuthMessage(o1, encoded)
 	if !assert.NoError(t, err2) {
 		return
 	}
-	if !reflect.DeepEqual(dst, value) {
-		t.Fatalf("Expected %v, got %v.", value, dst)
+	if !reflect.DeepEqual(v, value) {
+		t.Fatalf("Expected %v, got %v.", value, v)
 	}
-	var dst2 string
-	err3 := DecodeAuthMessage(o2, encoded, &dst2)
+	_, err3 := DecodeAuthMessage(o2, encoded)
 	if !assert.Error(t, err3) {
 		return
 	}
-	err4 := DecodeAuthMessage(o3, encoded, &dst2)
+	_, err4 := DecodeAuthMessage(o3, encoded)
 	if !assert.Error(t, err4) {
 		return
 	}
@@ -94,22 +90,20 @@ func TestMACWrongMessage(t *testing.T) {
 		Key: key,
 	}
 
-	var dst string
-
 	buf1 := new(bytes.Buffer)
-	err1 := DecodeAuthMessage(o, buf1.Bytes(), &dst)
+	_, err1 := DecodeAuthMessage(o, buf1.Bytes())
 	if !assert.Equal(t, errMACInvalid, err1) {
 		return
 	}
 
 	buf2 := base64Encode(GenerateRandomBytes(32))
-	err2 := DecodeAuthMessage(o, buf2, &dst)
+	_, err2 := DecodeAuthMessage(o, buf2)
 	if !assert.Equal(t, errMACInvalid, err2) {
 		return
 	}
 
 	buf3 := base64Encode(createMAC(key, []byte("")))
-	err3 := DecodeAuthMessage(o, buf3, &dst)
+	_, err3 := DecodeAuthMessage(o, buf3)
 	if !assert.Equal(t, errMACInvalid, err3) {
 		return
 	}
@@ -144,28 +138,4 @@ func TestEncoding(t *testing.T) {
 			return
 		}
 	}
-}
-
-func TestCustomType(t *testing.T) {
-	type MyStruct struct {
-		Field1 int64  `json:"field_1"`
-		Field2 string `json:"field_2"`
-		Field3 []byte `json:"field_3"`
-	}
-
-	o := &MACConfig{
-		Key: []byte("0123456789012345"),
-	}
-	src := &MyStruct{42, "bar", []byte("123123123123")}
-	encoded, _ := EncodeAuthMessage(o, src)
-
-	dst := &MyStruct{}
-	err := DecodeAuthMessage(o, encoded, dst)
-	if !assert.NoError(t, err) {
-		return
-	}
-
-	assert.Equal(t, dst.Field1, int64(42))
-	assert.Equal(t, dst.Field2, "bar")
-	assert.EqualValues(t, dst.Field3, []byte("123123123123"))
 }
