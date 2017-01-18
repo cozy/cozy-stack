9ccecedb60843a07292e97e22e4b5845daf2a428
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-01-18 13:54:00 +0100

    Do not use spec packages, and just used _test packages instead in same directory

--- web/apps/apps_test.go
+++ web/apps/apps_test.go
@@ -1,7 +1,7 @@
 // spec package is introduced to avoid circular dependencies since this
 // particular test requires to depend on routing directly to expose the API and
 // the APP server.
-package spec
+package apps_test
 
 import (
 	"bytes"
@@ -225,7 +225,7 @@ func TestServeAppsWithACode(t *testing.T) {
 
 func TestMain(m *testing.M) {
 	config.UseTestFile()
-	config.GetConfig().Assets = "../../../assets"
+	config.GetConfig().Assets = "../../assets"
 
 	tempdir, err := ioutil.TempDir("", "cozy-stack")
 	if err != nil {
