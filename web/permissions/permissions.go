c59a866ee3ad614fdbf23124c0826432b7c2f118
Romain <romain@cozycloud.cc>
2017-01-18 12:35:23 +0100

    Remove blackList
    will be reintroduced properly in nextPR

--- web/permissions/permissions.go
+++ web/permissions/permissions.go
@@ -2,12 +2,8 @@ package permissions
 
 import (
 	"errors"
-	"fmt"
-	"net/http"
 	"strings"
 
-	"github.com/cozy/cozy-stack/pkg/consts"
-	"github.com/cozy/cozy-stack/pkg/couchdb"
 	"github.com/cozy/cozy-stack/pkg/crypto"
 	"github.com/cozy/cozy-stack/pkg/instance"
 	"github.com/cozy/cozy-stack/pkg/permissions"
@@ -15,60 +11,6 @@ import (
 	"github.com/labstack/echo"
 	jwt "gopkg.in/dgrijalva/jwt-go.v3"
 )
-
-var readable = true
-var none = false
-
-var blackList = map[string]bool{
-	consts.Sessions:  none,
-	consts.Files:     readable,
-	consts.Instances: readable,
-}
-
-var writable = false
-var methodMap = map[string]bool{
-	http.MethodGet:    readable,
-	http.MethodPost:   writable,
-	http.MethodPut:    writable,
-	http.MethodPatch:  writable,
-	http.MethodDelete: writable,
-}
-
-func checkBlackList(c echo.Context, method, doctype string) error {
-	readabletype, inblacklist := blackList[doctype]
-	requestIsRead := methodMap[c.Request().Method]
-
-	if !inblacklist {
-		return nil
-	}
-
-	if requestIsRead && readabletype {
-		return nil
-	}
-
-	if requestIsRead && !readabletype {
-		return &echo.HTTPError{
-			Code:    http.StatusForbidden,
-			Message: fmt.Sprintf("reserved doctype %s unreadable", doctype),
-		}
-	}
-
-	return &echo.HTTPError{
-		Code:    http.StatusForbidden,
-		Message: fmt.Sprintf("reserved doctype %s unwritable", doctype),
-	}
-
-}
-
-// CheckDoc returns an error if the context doesnt allows the operation
-func CheckDoc(c echo.Context, method string, doc couchdb.Doc) error {
-	return checkBlackList(c, method, doc.DocType())
-}
-
-// CheckByID returns an error if the context doesnt allows the operation
-func CheckByID(c echo.Context, method, doctype, docid string) error {
-	return checkBlackList(c, method, doctype)
-}
 
 // keyPicker choose the proper instance key depending on token audience
 func keyPicker(i *instance.Instance) jwt.Keyfunc {
