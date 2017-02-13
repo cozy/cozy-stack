314a3aec8415ba4359c9e225e661bcdcd7c80dd5
Bruno Michel <bmichel@menfin.info>
2017-02-13 17:56:33 +0100

    Allow to use --appdir with several apps

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"os"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/cozy/cozy-stack/pkg/instance"
@@ -24,6 +25,15 @@ Use the --port and --host flags to change the listening option.
 If you are the developer of a client-side app, you can use --appdir
 to mount a directory as the application with the 'app' slug.
 `,
+	Example: `The most often, this command is used in its simple form:
+
+	$ cozy-stack serve
+
+But if you want to develop two apps in local (to test their interactions for
+example), you can use the --appsdir flag like this:
+
+	$ cozy-stack serve --appsdir appone:/path/to/app_one,apptwo:/path/to/app_two
+`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if !flagAllowRoot && os.Getuid() == 0 {
 			log.Errorf("Use --allow-root if you really want to start with the root user")
@@ -33,7 +43,19 @@ to mount a directory as the application with the 'app' slug.
 			return err
 		}
 		if flagAppdir != "" {
-			return web.ListenAndServeWithAppDir(flagAppdir)
+			apps := make(map[string]string)
+			for _, app := range strings.Split(flagAppdir, ",") {
+				parts := strings.Split(app, ":")
+				switch len(parts) {
+				case 1:
+					apps["app"] = parts[0]
+				case 2:
+					apps[parts[0]] = parts[1]
+				default:
+					return errors.New("Invalid appdir value")
+				}
+			}
+			return web.ListenAndServeWithAppDir(apps)
 		}
 		return web.ListenAndServe()
 	},
