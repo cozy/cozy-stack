4c151431ea4c29c62b44887dc41d6daf297689d9
Bruno Michel <bmichel@menfin.info>
2017-02-16 14:37:23 +0100

    Fix tests

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -2,8 +2,9 @@
 
 go run main.go serve &
 sleep 5
-go run main.go instances add --dev localhost:8080
+go run main.go instances add --dev --passphrase cozytest localhost:8080
 
+curl -H 'Accept: application/json' -H 'Content-Type: application/json' http://localhost:8080/auth/register -d '{ "redirect_uris": ["http://localhost/"], "client_name": "test", "software_id": "integration-test" }'
 export TEST_TOKEN=$(go run main.go instances token-oauth localhost:8080 test io.cozy.pouchtestobject)
 
 cd integration-tests/pouchdb
