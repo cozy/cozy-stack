175b475bbe4171a4baf0ae19897359cecde6b63b
Bruno Michel <bmichel@menfin.info>
2017-02-22 09:47:31 +0100

    Add comments to metadata

--- pkg/vfs/metadata.go
+++ pkg/vfs/metadata.go
@@ -6,16 +6,21 @@ import (
 	"github.com/xor-gate/goexif2/exif"
 )
 
-// TODO add comments and tests
+// TODO add tests
 
+// Metadata is a list of metadata specific to each mimetype:
+// id3 for music, exif for jpegs, etc.
 type Metadata map[string]interface{}
 
+// MetaExtractor is an interface for extracting metadata from a file
 type MetaExtractor interface {
 	io.WriteCloser
 	Abort(error)
 	Result() Metadata
 }
 
+// NewMetaExtractor returns an extractor for metadata if the mime type has one,
+// or null else
 func NewMetaExtractor(doc *FileDoc) *MetaExtractor {
 	// TODO png, gif, etc.
 	if doc.Mime == "image/jpg" {
@@ -25,11 +30,13 @@ func NewMetaExtractor(doc *FileDoc) *MetaExtractor {
 	return nil
 }
 
+// ExifExtractor is used to extract EXIF metadata from jpegs
 type ExifExtractor struct {
 	w *io.PipeWriter
 	r *io.PipeReader
 }
 
+// NewExifExtractor returns an extractor for EXIF metadata
 // TODO first, make it works
 // TODO then, use a goroutine to avoid having the whole file in memory
 func NewExifExtractor() *ExifExtractor {
@@ -38,18 +45,22 @@ func NewExifExtractor() *ExifExtractor {
 	return e
 }
 
+// Write is called to push some bytes to the extractor
 func (e *ExifExtractor) Write(p []byte) (n int, err error) {
 	return e.w.Write(p)
 }
 
+// Close is called when all the bytes has been pushed, to finalize the extraction
 func (e *ExifExtractor) Close() error {
 	return e.w.Close()
 }
 
+// Abort is called when the extractor can be discarded
 func (e *ExifExtractor) Abort(err error) {
 	e.w.CloseWithError(err)
 }
 
+// Result is called to get the extracted metadata
 func (e *ExifExtractor) Result() Metadata {
 	m := Metadata{}
 	x, err := exif.Decode(e.r)
