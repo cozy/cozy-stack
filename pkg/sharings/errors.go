e0ca23a2f30e4649c7986b2d60ca46ff3da3f2f5
Gara64 <paul@cozycloud.cc>
2017-02-24 11:16:05 +0100

    Add sharing reception handler

--- pkg/sharings/errors.go
+++ pkg/sharings/errors.go
@@ -7,4 +7,8 @@ var (
 	ErrBadSharingType = errors.New("Invalid sharing type")
 	//ErrRecipientDoesNotExist is used when the given recipient does not exist
 	ErrRecipientDoesNotExist = errors.New("Recipient with given ID does not exist")
+	//ErrMissingScope is used when a request is missing the mandatory scope
+	ErrMissingScope = errors.New("The scope parameter is mandatory")
+	//ErrMissingState is used when a request is missing the mandatory state
+	ErrMissingState = errors.New("The state parameter is mandatory")
 )
--- web/sharings/sharings.go
+++ web/sharings/sharings.go
@@ -1,13 +1,61 @@
 package sharings
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/cozy/cozy-stack/pkg/permissions"
 	"github.com/cozy/cozy-stack/pkg/sharings"
 	"github.com/cozy/cozy-stack/web/jsonapi"
 	"github.com/cozy/cozy-stack/web/middlewares"
 	"github.com/labstack/echo"
 )
+
+// SharingRequest handles a sharing request from the recipient side
+// It creates a tempory sharing document, waiting for the recipient answer
+func SharingRequest(c echo.Context) error {
+	//Get the permissions and sharing id
+	scope := c.QueryParam("scope")
+	if scope == "" {
+		return wrapErrors(sharings.ErrMissingScope)
+	}
+	state := c.QueryParam("state")
+	if state == "" {
+		return wrapErrors(sharings.ErrMissingState)
+	}
+	sharingType := c.QueryParam("sharing_type")
+	//TODO : Check sharing type integrity
+	if sharingType == "" {
+		return wrapErrors(sharings.ErrBadSharingType)
+	}
+	fmt.Printf("scope : %v\n", scope)
+	permissions, err := permissions.UnmarshalScopeString(scope)
+	if err != nil {
+		fmt.Println("error...")
+		return err
+	}
+	fmt.Printf("perm : %+v", permissions)
+
+	sharing := &sharings.Sharing{
+		SharingType: sharingType,
+		SharingID:   state,
+		Permissions: permissions,
+		Owner:       false,
+	}
+
+	instance := middlewares.GetInstance(c)
+	_, err = sharings.Create(instance, sharing)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("state : %v", sharing.SharingID)
+
+	//TODO call the OAuth authorize to display the permissions
+	//TODO return the permission html
+
+	return jsonapi.Data(c, http.StatusCreated, sharing, nil)
+}
 
 // CreateSharing initializes a sharing by creating the associated document
 func CreateSharing(c echo.Context) error {
@@ -34,6 +82,7 @@ func CreateSharing(c echo.Context) error {
 func Routes(router *echo.Group) {
 	// API Routes
 	router.POST("/", CreateSharing)
+	router.GET("/request", SharingRequest)
 }
 
 // wrapErrors returns a formatted error
@@ -43,6 +92,10 @@ func wrapErrors(err error) error {
 		return jsonapi.InvalidParameter("sharing_type", err)
 	case sharings.ErrRecipientDoesNotExist:
 		return jsonapi.NotFound(err)
+	case sharings.ErrMissingScope:
+		return jsonapi.BadRequest(err)
+	case sharings.ErrMissingState:
+		return jsonapi.BadRequest(err)
 	}
 	return err
 }
