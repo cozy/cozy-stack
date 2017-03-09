6a01f80e0645557af54683da7a637223ca087784
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-03-09 17:06:37 +0100

    Hotfix of the pkg/apps tests after adding index on io.cozy.sahrings

--- pkg/apps/installer_test.go
+++ pkg/apps/installer_test.go
@@ -514,7 +514,13 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
-	err = couchdb.DefineIndexes(c, consts.Indexes)
+	err = couchdb.DefineIndexes(c, consts.IndexesByDoctype(consts.Files))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = couchdb.DefineIndexes(c, consts.IndexesByDoctype(consts.Permissions))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
