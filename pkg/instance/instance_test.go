a6fab222c574e5d63b266c4bcbde717fa1db5e87
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-03-27 10:39:18 +0200

    Fix pkg/instance tests

--- pkg/instance/instance_test.go
+++ pkg/instance/instance_test.go
@@ -368,6 +368,10 @@ func TestGetFs(t *testing.T) {
 	instance := Instance{
 		Domain: "test-provider.cozycloud.cc",
 	}
+	err := instance.makeVFS()
+	if !assert.NoError(t, err) {
+		return
+	}
 	storage := instance.VFS()
 	assert.NotNil(t, storage, "the instance should have a memory storage provider")
 }
