972690d3d82beec1302bfc2cd570254fa6420842
Gara64 <paul@cozycloud.cc>
2017-03-28 18:56:47 +0200

    Add sharing worker
    This worker will be used for sharing sync

---
+++ pkg/jobs/workers/sharing.go
@@ -0,0 +1,29 @@
+package workers
+
+import (
+	"context"
+	"time"
+
+	"github.com/cozy/cozy-stack/pkg/couchdb"
+	"github.com/cozy/cozy-stack/pkg/jobs"
+)
+
+func init() {
+	jobs.AddWorker("sharing", &jobs.WorkerConfig{
+		Concurrency:  4,
+		MaxExecCount: 3,
+		Timeout:      10 * time.Second,
+		WorkerFunc:   SharingUpdates,
+	})
+}
+
+// SharingUpdates checks the shared doc updates
+func SharingUpdates(ctx context.Context, m *jobs.Message) error {
+	doc := &couchdb.JSONDoc{}
+	err := doc.UnmarshalJSON(m.Data)
+	if err != nil {
+		return err
+	}
+	//TODO : call a senddoc worker to send updated files
+	return nil
+}
