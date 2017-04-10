0630a9ed470fa539599bb0bb0add073e0447c770
Bruno Michel <bmichel@menfin.info>
2017-04-10 10:26:34 +0200

    Add a CSP whitelist, with Open Street Map and Mapbox

--- web/routing.go
+++ web/routing.go
@@ -116,7 +116,7 @@ func SetupAppsHandler(appsHandler echo.HandlerFunc) echo.HandlerFunc {
 		CSPDefaultSrc: []middlewares.CSPSource{middlewares.CSPSrcSelf, middlewares.CSPSrcParent},
 		CSPStyleSrc:   []middlewares.CSPSource{middlewares.CSPSrcSelf, middlewares.CSPSrcParent, middlewares.CSPUnsafeInline},
 		CSPFontSrc:    []middlewares.CSPSource{middlewares.CSPSrcSelf, middlewares.CSPSrcData, middlewares.CSPSrcParent},
-		CSPImgSrc:     []middlewares.CSPSource{middlewares.CSPSrcSelf, middlewares.CSPSrcData, middlewares.CSPSrcBlob, middlewares.CSPSrcParent},
+		CSPImgSrc:     []middlewares.CSPSource{middlewares.CSPSrcSelf, middlewares.CSPSrcData, middlewares.CSPSrcBlob, middlewares.CSPSrcParent, middlewares.CSPSrcWhitelist},
 		CSPFrameSrc:   []middlewares.CSPSource{middlewares.CSPSrcSiblings},
 		XFrameOptions: middlewares.XFrameDeny,
 	})
