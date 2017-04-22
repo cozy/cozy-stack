239550f659a9d0454bcff1f223073abf8f89059b
Gara64 <paul@cozycloud.cc>
2017-04-22 17:59:41 +0200

    json api response

--- web/sharings/sharings.go
+++ web/sharings/sharings.go
@@ -205,7 +205,8 @@ func AddSharingRecipient(c echo.Context) error {
 		return wrapErrors(err)
 	}
 
-	return wrapErrors(err)
+	return jsonapi.Data(c, http.StatusOK, &apiSharing{sharing}, nil)
+
 }
 
 // RecipientRefusedSharing is called when the recipient refused the sharing.
