a020a948212e1fa3e9bc3e85c9881acfcd30788d
Bruno Michel <bmichel@menfin.info>
2017-04-26 11:20:17 +0200

    Start the job system after Swift connexion was initialized, as a job may need it

--- pkg/stack/main.go
+++ pkg/stack/main.go
@@ -8,16 +8,16 @@ import (
 
 // Start is used to initialize all the
 func Start() error {
-	if err := jobs.StartSystem(); err != nil {
-		return err
-	}
-
 	// Init the main global connection to the swift server
 	fsURL := config.FsURL()
 	if fsURL.Scheme == "swift" {
 		if err := vfsswift.InitConnection(fsURL); err != nil {
 			return err
 		}
+	}
+
+	if err := jobs.StartSystem(); err != nil {
+		return err
 	}
 
 	return nil
