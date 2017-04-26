3fe0e30fbbd2d8dc903bf3e3de96aff6d14ba852
Romain <romain@cozycloud.cc>
2017-04-26 19:48:21 +0200

    add storage for sessions code

--- pkg/sessions/code.go
+++ pkg/sessions/code.go
@@ -9,6 +9,9 @@ import (
 // CodeLen is the number of random bytes used for the session codes
 const CodeLen = 16
 
+// CodeTTL is the lifetime of a SessionCode
+const CodeTTL = 1 * time.Minute
+
 // A Code is used to transfer a session from the domain with the stack to a
 // subdomain of an application, when the flat subdomains structure is used.
 // The code is valid:
@@ -22,44 +25,21 @@ type Code struct {
 	ExpiresAt int64
 }
 
-// The list of pending codes
-var codes []*Code
-
 // BuildCode creates a session code for the given session and app
 func BuildCode(sessionID, app string) *Code {
 	value := crypto.GenerateRandomBytes(CodeLen)
 	value = crypto.Base64Encode(value)
-	expires := time.Now().UTC().Add(1 * time.Minute).Unix()
 	code := &Code{
 		Value:     string(value),
 		SessionID: sessionID,
 		AppHost:   app,
-		ExpiresAt: expires,
 	}
-	codes = append(codes, code)
+	getStorage().Add(code)
 	return code
 }
 
 // FindCode tries to find a valid pending code.
 // It also clears the pending codes of all the expired codes.
 func FindCode(value, app string) *Code {
-	var found *Code
-	if len(codes) == 0 {
-		return nil
-	}
-
-	// See https://github.com/golang/go/wiki/SliceTricks#filtering-without-allocating
-	validCodes := codes[:0]
-	now := time.Now().UTC().Unix()
-	for _, c := range codes {
-		if now < c.ExpiresAt {
-			if c.Value == value && c.AppHost == app {
-				found = c
-			} else {
-				validCodes = append(validCodes, c)
-			}
-		}
-	}
-	codes = validCodes
-	return found
+	return getStorage().FindAndDelete(value, app)
 }
