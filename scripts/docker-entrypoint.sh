7a4a8dbd15de71184142f2cbd5321507781858f1
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-05-02 14:56:07 +0200

    Docker: fix apps warning when using multi-apps

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -9,13 +9,21 @@ MailHog 2> /dev/null 1> /dev/null &
 if [ -f "/data/cozy-app/manifest.webapp" ]; then
 	appdir="/data/cozy-app"
 else
+	show_warn=false
+	for i in /data/cozy-app/*; do
+		if [ ! -f "${i}/manifest.webapp" ]; then
+			show_warn=true
+		fi
+	done
 	appdir=""
-	>&2 echo -e ""
-	>&2 echo -e "WARNING:"
-	>&2 echo -e "  No manifest.webapp file has been found in the mounted"
-	>&2 echo -e "  directory /data/cozy-app. The stack will be started"
-	>&2 echo -e "  without serving any local application."
-	>&2 echo -e ""
+	if $show_warn; then
+		>&2 echo -e ""
+		>&2 echo -e "WARNING:"
+		>&2 echo -e "  No manifest.webapp file has been found in the mounted"
+		>&2 echo -e "  directory /data/cozy-app. The stack will be started"
+		>&2 echo -e "  without serving any local application."
+		>&2 echo -e ""
+	fi
 fi
 
 /usr/bin/cozy-app-dev.sh \
