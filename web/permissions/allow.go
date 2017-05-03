d322d442541da15472d7f37fe3500ef44e84d8f7
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-05-03 18:22:38 +0200

    Fix sourceID for application

--- web/permissions/allow.go
+++ web/permissions/allow.go
@@ -77,7 +77,7 @@ func AllowInstallApp(c echo.Context, appType apps.AppType, v permissions.Verb) e
 	switch appType {
 	case apps.Konnector:
 		docType = consts.Konnectors
-		sourceID = consts.Konnectors + "/" + consts.DataConnectSlug
+		sourceID = consts.Apps + "/" + consts.DataConnectSlug
 	case apps.Webapp:
 		docType = consts.Apps
 		sourceID = consts.Apps + "/" + consts.StoreSlug
