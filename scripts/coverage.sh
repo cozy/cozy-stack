dbd0f52d40a1d4db3eb9f54d66728c178de868a3
Bruno Michel <bmichel@menfin.info>
2017-05-10 10:44:41 +0200

    Ignore warnings in coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,7 +4,12 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./pkg/... ./web/...); do
-	go test -coverprofile=profile.out -covermode=count -coverpkg=./pkg/...,./web/... $d
+	go test \
+		-coverprofile=profile.out \
+		-covermode=count \
+		-coverpkg=./pkg/...,./web/... \
+		"$d" \
+		2>&1 | grep -v 'warning: no packages being tested depend on github.com/cozy/cozy-stack'
 	if [ -f profile.out ]; then
 		cat profile.out >> coverage.txt
 		rm profile.out
