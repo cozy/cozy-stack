5d7a3459a836ab06a1d2c3610833c48131b2a798
KOCOGLU aylin <kocoglu@informatique.univ-paris-diderot.fr>
2017-06-14 17:32:25 +0200

    Send mail and remove the tarball after export

--- web/imexport/imexport.go
+++ web/imexport/imexport.go
@@ -1,25 +1,55 @@
 package imexport
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 
 	"github.com/cozy/cozy-stack/pkg/imexport"
+	"github.com/cozy/cozy-stack/pkg/jobs"
+	"github.com/cozy/cozy-stack/pkg/utils"
+	workers "github.com/cozy/cozy-stack/pkg/workers/mails"
 	"github.com/cozy/cozy-stack/web/middlewares"
 	"github.com/cozy/echo"
 )
 
 func export(c echo.Context) error {
-	w, err := os.Create("cozy.tar.gz")
+	instance := middlewares.GetInstance(c)
+	fs := instance.VFS()
+
+	domain := instance.Domain
+
+	id := utils.RandomString(16)
+
+	w, err := os.Create(fmt.Sprintf("%s-%s.tar.gz", domain, id))
 	if err != nil {
 		return err
 	}
 	defer w.Close()
 
-	instance := middlewares.GetInstance(c)
-	fs := instance.VFS()
-
 	err = imexport.Tardir(w, fs)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(instance.Domain, c.Path())
+
+	mailBody := fmt.Sprintf("Bonjour, vous pouvez dès à présent récupérer vos documents sur le lien suivant : http://%s%s%s-%s",
+		domain, c.Path(), domain, id)
+	msg, _ := jobs.NewMessage("json", workers.Options{
+		Mode:    workers.ModeNoReply,
+		Subject: "Cozy: vos documents sont prêts",
+		Parts: []*workers.Part{
+			{
+				Type: "text/plain",
+				Body: mailBody,
+			},
+		},
+	})
+
+	context := jobs.NewWorkerContext(instance.Domain, "abcd")
+	err = workers.SendMail(context, msg)
 	if err != nil {
 		return err
 	}
@@ -29,9 +59,39 @@ func export(c echo.Context) error {
 	})
 }
 
+func exportDir(c echo.Context) error {
+	domId := c.Param("domain-id")
+	fmt.Println(domId)
+
+	src, err := os.Open(fmt.Sprintf("%s.tar.gz", domId))
+	if err != nil {
+		return err
+	}
+	dst, err := os.Create("cozy.tar.gz")
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(fmt.Sprintf("%s.tar.gz", domId))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "bienvenue sur la super page bis",
+	})
+}
+
 // Routes sets the routing for export
 func Routes(router *echo.Group) {
 	router.GET("/", export)
 	router.HEAD("/", export)
+
+	router.GET("/:domain-id", exportDir)
 
 }
