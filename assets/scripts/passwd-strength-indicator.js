d293167ec2fce7155fe2a110d3f0cfdb94a0142e
m4dz <code@m4dz.net>
2017-07-04 18:23:17 +0200

    [refactor] externalize password helper

--- assets/scripts/passwd-strength-indicator.js
+++ assets/scripts/passwd-strength-indicator.js
@@ -1,65 +1,9 @@
 (function (window, document) {
   const indicator = document.getElementById('password-strength')
-  const submit = document.getElementById('login-submit')
 
   document.getElementById('password').addEventListener('input', function(event) {
-    const strength = getStrength(event.target.value)
+    const strength = window.password.getStrength(event.target.value)
     indicator.value = parseInt(strength.percentage, 10)
     indicator.setAttribute('class', `pw-${strength.label}`)
-    submit[strength.label == 'weak' ? 'setAttribute' : 'removeAttribute']('disabled', '')
   }, false)
-
-  // Return given password strength as an object {percentage, label}
-  getStrength = function (password) {
-    if (!password && password !== '') {
-      throw new Error('password parameter is missing')
-    }
-    if (!password.length) {
-      return {percentage: 0, label: 'weak'}
-    }
-
-    const charsets = [
-      // upper
-      { regexp: /[A-Z]/g, size: 26 },
-      // lower
-      { regexp: /[a-z]/g, size: 26 },
-      // digit
-      { regexp: /[0-9]/g, size: 10 },
-      // special
-      { regexp: /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/g, size: 30 }
-    ]
-
-    const possibleChars = charsets.reduce(function (possibleChars, charset) {
-      if (charset.regexp.test(password)) possibleChars += charset.size
-      return possibleChars
-    }, 0)
-
-    const passwordStrength =
-    (Math.log(Math.pow(possibleChars, password.length)) / (Math.log(2)))
-
-    // levels
-    const _at33percent = 50
-    const _at66percent = 100
-    const _at100percent = 150
-
-    let strengthLabel = ''
-    let strengthPercentage = 0
-
-    // between 0% and 33%
-    if (passwordStrength <= _at33percent) {
-      strengthPercentage = passwordStrength * 33 / _at33percent
-      strengthLabel = 'weak'
-    } else if (passwordStrength > _at33percent && passwordStrength <= _at66percent) {
-      // between 33% and 66%
-      strengthPercentage = passwordStrength * 66 / _at66percent
-      strengthLabel = 'moderate'
-    } else {
-      // passwordStrength > 192
-      strengthPercentage = passwordStrength * 100 / _at100percent
-      if (strengthPercentage > 100) strengthPercentage = 100
-      strengthLabel = 'strong'
-    }
-
-    return {percentage: strengthPercentage, label: strengthLabel}
-  }
 })(window, document)
