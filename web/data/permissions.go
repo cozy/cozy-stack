689568018f6e236b1824b65f935517c545b02953
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-08-02 14:45:37 +0200

    Set notifications doctype readable

--- web/data/permissions.go
+++ web/data/permissions.go
@@ -25,6 +25,7 @@ var blackList = map[string]bool{
 	consts.KonnectorResults: readable,
 	consts.Files:            readable,
 	consts.Jobs:             readable,
+	consts.Notifications:    readable,
 	consts.Queues:           readable,
 	consts.Triggers:         readable,
 	consts.RemoteRequests:   readable,
