898a3f1676867621099789022c200c704adb5695
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-08-25 08:35:25 +0200

    Use a modified domain version as the instance dir name on Windows
    Some domain name characters are invalid in Windows dir names.

---
+++ pkg/instance/posix.go
@@ -0,0 +1,9 @@
+// +build linux darwin
+
+package instance
+
+// DirName returns the name of the subdirectory where instance data are stored.
+// On Posix systems, it's the instance domain name.
+func (i *Instance) DirName() string {
+	return i.Domain
+}
