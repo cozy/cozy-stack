e2476902a0c130e1a86bd37c55b65a6471597b42
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-09-03 18:45:39 +0200

    Disable CSP from config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -311,6 +311,7 @@ func UseViper(v *viper.Viper) error {
 		},
 		Contexts:   v.GetStringMap("contexts"),
 		Registries: regs,
+		DisableCSP: v.GetBool("disable_csp"),
 	}
 
 	loggerRedis := NewRedisConfig(v.GetString("log.redis"))
