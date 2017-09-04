22d7ea4a40f472d565f3d9402846dbcd7b735fc3
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-09-04 12:47:36 +0200

    Named return

--- pkg/apps/fetcher_file.go
+++ pkg/apps/fetcher_file.go
@@ -38,7 +38,7 @@ func (f *fileFetcher) FetchManifest(src *url.URL) (io.ReadCloser, error) {
 	return r, nil
 }
 
-func (f *fileFetcher) Fetch(src *url.URL, fs Copier, man Manifest) error {
+func (f *fileFetcher) Fetch(src *url.URL, fs Copier, man Manifest) (err error) {
 	version := man.Version() + "-" + utils.RandomString(10)
 	man.SetVersion(version)
 	exists, err := fs.Start(man.Slug(), man.Version())
