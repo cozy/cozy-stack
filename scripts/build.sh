24ddabd889d9672fc2e6e6f7d6ee3881657c69e8
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-10-06 13:25:46 +0200

    Revert "Fail build.sh assets when transifex fails"
    This reverts commit aec3b734f30fc71cc53e9f111efe6989e8d9db89.

--- scripts/build.sh
+++ scripts/build.sh
@@ -181,7 +181,7 @@ do_deploy() {
 
 do_assets() {
 	clean_assets
-	tx -r "${WORK_DIR}" pull -a
+	tx -r "${WORK_DIR}" pull -a || echo "Do you have configured transifex?"
 	prepare_assets
 	printf "executing go generate... "
 	go get -u github.com/rakyll/statik
