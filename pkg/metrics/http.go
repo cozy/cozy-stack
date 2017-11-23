b31c8ff9e9e4094d094d861f6b7c3d314b9bda6d
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-11-23 15:31:58 +0100

    Set objectives

--- pkg/metrics/http.go
+++ pkg/metrics/http.go
@@ -11,6 +11,8 @@ var HTTPTotalDurations = prometheus.NewSummaryVec(
 		Name:      "total_duration",
 
 		Help: "Durations of http requests, labelled by method and status code",
+
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 	},
 	[]string{"method", "code"},
 )
