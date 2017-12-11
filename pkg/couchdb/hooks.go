254056c96031e845cb25a4b1a39e3c77784f2efb
Bruno Michel <bmichel@menfin.info>
2017-12-11 13:25:42 +0100

    Refactor hooks package to use a key type for map

--- pkg/couchdb/hooks.go
+++ pkg/couchdb/hooks.go
@@ -6,7 +6,12 @@ import "github.com/cozy/cozy-stack/pkg/realtime"
 // A hook can block the event by returning an error
 type listener func(domain string, doc Doc, old Doc) error
 
-var hooks map[string]map[string][]listener
+type key struct {
+	DocType string
+	Event   string
+}
+
+var hooks map[key][]listener
 
 // Events to hook into
 const (
@@ -17,13 +22,11 @@ const (
 
 // Run runs all hooks for the given event.
 func runHooks(domain, event string, doc Doc, old Doc) error {
-	if m, ok := hooks[doc.DocType()]; ok {
-		if hs, ok := m[event]; ok {
-			for _, h := range hs {
-				err := h(domain, doc, old)
-				if err != nil {
-					return err
-				}
+	if hs, ok := hooks[key{doc.DocType(), event}]; ok {
+		for _, h := range hs {
+			err := h(domain, doc, old)
+			if err != nil {
+				return err
 			}
 		}
 	}
@@ -34,18 +37,12 @@ func runHooks(domain, event string, doc Doc, old Doc) error {
 // Useful for special doctypes cleanup
 func AddHook(doctype, event string, hook listener) {
 	if hooks == nil {
-		hooks = make(map[string]map[string][]listener)
+		hooks = make(map[key][]listener)
 	}
-	m, ok := hooks[doctype]
-	if !ok {
-		m = make(map[string][]listener)
-		hooks[doctype] = m
-	}
-	hs, ok := m[event]
+	k := key{doctype, event}
+	hs, ok := hooks[k]
 	if !ok {
 		hs = make([]listener, 0)
-		m[event] = hs
 	}
-	m[event] = append(hs, hook)
-
+	hooks[k] = append(hs, hook)
 }
