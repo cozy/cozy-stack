25e9bb2d98a6defdb331def50debf5dbd65053c1
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2018-01-20 17:34:39 +0100

    More tests on credentials

--- pkg/accounts/credentials_test.go
+++ pkg/accounts/credentials_test.go
@@ -14,18 +14,32 @@ import (
 )
 
 func TestEncryptDecrytCredentials(t *testing.T) {
-	encryptedCreds, err := EncryptCredentials("me@mycozy.cloud", "fzEE6HFWsSp8jP")
+	encryptedCreds1, err := EncryptCredentials("me@mycozy.cloud", "fzEE6HFWsSp8jP")
 	if !assert.NoError(t, err) {
 		return
 	}
-
-	login, password, err := DecryptCredentials(encryptedCreds)
+	encryptedCreds2, err := EncryptCredentials("me@mycozy.cloud", "fzEE6HFWsSp8jP")
 	if !assert.NoError(t, err) {
 		return
 	}
+	assert.False(t, bytes.Equal(encryptedCreds1, encryptedCreds2))
 
-	assert.Equal(t, "me@mycozy.cloud", login)
-	assert.Equal(t, "fzEE6HFWsSp8jP", password)
+	{
+		login, password, err := DecryptCredentials(encryptedCreds1)
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, "me@mycozy.cloud", login)
+		assert.Equal(t, "fzEE6HFWsSp8jP", password)
+	}
+	{
+		login, password, err := DecryptCredentials(encryptedCreds2)
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, "me@mycozy.cloud", login)
+		assert.Equal(t, "fzEE6HFWsSp8jP", password)
+	}
 }
 
 func TestEncryptDecrytUTF8Credentials(t *testing.T) {
@@ -82,6 +96,51 @@ func TestDecryptCredentialsRandom(t *testing.T) {
 		copy(encryptedWithHeader[len(cipherHeader):], encrypted)
 		_, _, err := DecryptCredentials(encryptedWithHeader)
 		assert.Error(t, err)
+	}
+}
+
+func TestRandomBitFlips(t *testing.T) {
+	original, err := EncryptCredentials("toto@titi.com", "X3hVYLJLRiUyCs")
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	flipped := make([]byte, len(original))
+	copy(flipped, original)
+	login, passwd, err := DecryptCredentials(flipped)
+	if !assert.NoError(t, err) {
+		return
+	}
+	assert.Equal(t, "toto@titi.com", login)
+	assert.Equal(t, "X3hVYLJLRiUyCs", passwd)
+
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 100000; i++ {
+		copy(flipped, original)
+		flipsLen := rng.Intn(30) + 1
+		flipsSet := make([]int, 0, flipsLen)
+		for len(flipsSet) < flipsLen {
+			flipValue := rng.Intn(len(original) * 8)
+			flipFound := false
+			for j := 0; j < len(flipsSet); j++ {
+				if flipsSet[j] == flipValue {
+					flipFound = true
+					break
+				}
+			}
+			if !flipFound {
+				flipsSet = append(flipsSet, flipValue)
+			}
+		}
+		for _, flipValue := range flipsSet {
+			mask := byte(0x1 << uint(flipValue%8))
+			flipped[flipValue/8] ^= mask
+		}
+		_, _, err := DecryptCredentials(flipped)
+		if !assert.Error(t, err) {
+			t.Fatalf("Failed with flips %v", flipsSet)
+			return
+		}
 	}
 }
 
