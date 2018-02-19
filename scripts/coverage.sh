b622ad6882a682ab8c1d635b36dbe5f56d2f9ceb
Bruno Michel <bmichel@menfin.info>
2018-02-19 09:37:09 +0100

    Generate coverage only on go 1.10 on travis

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,34 +1,10 @@
 #!/usr/bin/env bash
 
-set -o pipefail
-
-echo "" > coverage.txt
-failed=false
-gosrc="$(go env GOPATH)/src"
-
-for d in $(go list ./pkg/... ./web/...); do
-	if test -n "$(find $gosrc/$d -maxdepth 1 -name '*_test.go' -print -quit)"; then
-		go test \
-			-coverprofile=profile.out \
-			-covermode=count \
-			-coverpkg=./pkg/...,./web/... \
-			-timeout 2m \
-			"$d" \
-			2>&1 | grep -v 'warning: no packages being tested depend on github.com/cozy/cozy-stack'
-		res=$?
-		if [ $res -eq 0 ]; then
-			if [ -f profile.out ]; then
-				cat profile.out >> coverage.txt
-				rm profile.out
-			fi
-		else
-			failed=true
-		fi
-	fi
-done
-
-if [ "$failed" = true ]; then
-	exit 1
-else
-	exit 0
-fi
+# See https://golang.org/doc/go1.10#test
+go test \
+	-coverprofile=coverage.txt \
+	-covermode=count \
+	-coverpkg=./pkg/...,./web/... \
+	-vet=off \
+	-timeout 2m \
+	./... \
