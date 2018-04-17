23f8a2531064cc376e5d345ecd78762d4e28bd7c
Bruno Michel <bmichel@menfin.info>
2018-04-17 18:35:07 +0200

    Add some debug logs in pkg/sharing

--- pkg/sharing/files.go
+++ pkg/sharing/files.go
@@ -184,11 +184,13 @@ func (s *Sharing) ApplyBulkFiles(inst *instance.Instance, docs DocsList) error {
 		var ref *SharedRef
 		err := couchdb.GetDoc(inst, consts.Shared, consts.Files+"/"+id, ref)
 		if err != nil && !couchdb.IsNotFoundError(err) {
+			inst.Logger().Debugf("[bulk_files] Error on finding doc: %s", err)
 			return err
 		}
 		// TODO it's only for directory currently, code needs to be adapted for files
 		doc, err := fs.DirByID(id) // TODO DirOrFileByID
 		if err != nil && err != os.ErrNotExist {
+			inst.Logger().Debugf("[bulk_files] Error on finding ref: %s", err)
 			return err
 		}
 		if ref == nil && doc == nil {
@@ -241,15 +243,17 @@ func copyTagsAndDatesToDir(target map[string]interface{}, dir *vfs.DirDoc) {
 func (s *Sharing) CreateDir(inst *instance.Instance, target map[string]interface{}) error {
 	name, ok := target["name"].(string)
 	if !ok {
+		inst.Logger().Debugf("[bulk_files] Missing name: %#v", target)
 		return ErrInternalServerError
 	}
 	rev, ok := target["_rev"].(string)
 	if !ok {
-		// TODO add logs or better error
+		inst.Logger().Debugf("[bulk_files] Missing _rev: %#v", target)
 		return ErrInternalServerError
 	}
 	revisions, ok := target["_revisions"].(map[string]interface{})
 	if !ok {
+		inst.Logger().Debugf("[bulk_files] Missing _revisions: %#v", target)
 		return ErrInternalServerError
 	}
 	indexer := NewSharingIndexer(inst, &bulkRevs{
@@ -264,24 +268,31 @@ func (s *Sharing) CreateDir(inst *instance.Instance, target map[string]interface
 		parent, err = fs.DirByID(dirID)
 		// TODO better handling of this conflict
 		if err != nil {
+			inst.Logger().Debugf("[bulk_files] Conflict for parent: %s", err)
 			return err
 		}
 	} else {
 		parent, err = s.GetSharingDir(inst)
 		if err != nil {
+			inst.Logger().Debugf("[bulk_files] Error on get sharing dir: %s", err)
 			return err
 		}
 	}
 
 	dir, err := vfs.NewDirDocWithParent(name, parent, nil)
 	if err != nil {
+		inst.Logger().Debugf("[bulk_files] Cannot initialize dir doc: %s", err)
 		return err
 	}
 	dir.SetID(target["_id"].(string))
 	copyTagsAndDatesToDir(target, dir)
 	// TODO referenced_by
 	// TODO manage conflicts
-	return fs.CreateDir(dir)
+	if err := fs.CreateDir(dir); err != nil {
+		inst.Logger().Debugf("[bulk_files] Cannot create dir: %s", err)
+		return err
+	}
+	return nil
 }
 
 // UpdateDir updates a directory on this cozy to reflect a change on another
