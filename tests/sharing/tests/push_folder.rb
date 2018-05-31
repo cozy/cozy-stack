2dd516c2627211bd27445f69f45c70ca609bab85
Bruno Michel <bmichel@menfin.info>
2018-05-31 17:28:29 +0200

    Merge metadata and push_folder tests

--- tests/sharing/tests/push_folder.rb
+++ tests/sharing/tests/push_folder.rb
@@ -2,6 +2,15 @@ require_relative '../boot'
 require 'minitest/autorun'
 require 'pry-rescue/minitest' unless ENV['CI']
 
+def assert_same_thumbs(base_path_a, id_a, base_path_b, id_b)
+  thumbs_a = Dir["#{base_path_a}/*"]
+  thumbs_a.each do |thumb_a|
+    suffix = thumb_a.split(id_a).last
+    thumb_b = File.join base_path_b, "#{id_b}#{suffix}"
+    assert_empty Helpers.fsdiff(thumb_a, thumb_b)
+  end
+end
+
 describe "A folder" do
   Helpers.scenario "push_folder"
   Helpers.start_mailhog
@@ -16,9 +25,9 @@ describe "A folder" do
     # Create the folder with a file
     folder = Folder.create inst
     folder.couch_id.wont_be_empty
-    file = "../fixtures/wet-cozy_20160910__©M4Dz.jpg"
-    opts = CozyFile.options_from_fixture(file, dir_id: folder.couch_id)
-    CozyFile.create inst, opts
+    file_path = "../fixtures/wet-cozy_20160910__©M4Dz.jpg"
+    opts = CozyFile.options_from_fixture(file_path, dir_id: folder.couch_id)
+    file = CozyFile.create inst, opts
 
     # Create the sharing
     contact = Contact.create inst, givenName: recipient_name
@@ -43,5 +52,24 @@ describe "A folder" do
                    Helpers::SHARED_WITH_ME, sharing.rules.first.title
     diff = Helpers.fsdiff da, db
     diff.must_be_empty
+
+    # Check the metadata are the same
+    file = CozyFile.find inst, file.couch_id
+    file_path = CGI.escape "/#{Helpers::SHARED_WITH_ME}/#{folder.name}/#{file.name}"
+    file_recipient = CozyFile.find_by_path inst_recipient, file_path
+    assert_equal file.md5sum, file_recipient.md5sum
+    assert_equal file.mime, file_recipient.mime
+    assert_equal file.class, file_recipient.class
+    assert_equal file.size, file_recipient.size
+    assert_equal file.executable, file_recipient.executable
+    refute_nil file.metadata
+    refute_nil file_recipient.metadata
+    assert_equal file.metadata, file_recipient.metadata
+
+    short_id_a = file.couch_id.slice(0..3)
+    short_id_b = file_recipient.couch_id.slice(0..3)
+    base_path_a = File.join Helpers.current_dir, inst.domain, ".thumbs", short_id_a
+    base_path_b = File.join Helpers.current_dir, inst_recipient.domain, ".thumbs", short_id_b
+    assert_same_thumbs base_path_a, file.couch_id, base_path_b, file_recipient.couch_id
   end
 end
