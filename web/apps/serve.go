e554250283d24c56bb5fe57b8db4c464d76b8fd0
Bruno Michel <bmichel@menfin.info>
2019-02-19 09:50:28 +0100

    Add a fallback on the default context for robots.txt

--- web/apps/serve.go
+++ web/apps/serve.go
@@ -173,6 +173,10 @@ func ServeAppFile(c echo.Context, i *instance.Instance, fs apps.FileServer, app 
 					_, err = io.Copy(c.Response(), f.Reader())
 					return err
 				}
+				if f, ok := statikfs.Get("/robots.txt", ""); ok {
+					_, err = io.Copy(c.Response(), f.Reader())
+					return err
+				}
 			}
 			return echo.NewHTTPError(http.StatusNotFound, "Asset not found")
 		}
