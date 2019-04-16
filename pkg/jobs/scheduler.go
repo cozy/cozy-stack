d72c975da34a2529540606eacb3da8b8164828c0
maximemenager <43468959+maximemenager@users.noreply.github.com>
2019-04-16 16:20:30 +0200

    Set the trigger last job informations earlier (#1829)
    In some cases, when no job has an "Errored" or "Done" state, the job is
    skipped, preventing the status to be updated. In that case, the status
    was set by default to "Done". This commit updates the status of the
    trigger before checking the job state.

--- pkg/jobs/scheduler.go
+++ pkg/jobs/scheduler.go
@@ -225,6 +225,16 @@ func GetTriggerState(db prefixer.Prefixer, triggerID string) (*TriggerState, err
 	for i := len(js) - 1; i >= 0; i-- {
 		j := js[i]
 		startedAt := &j.StartedAt
+
+		state.Status = j.State
+		state.LastExecution = startedAt
+		state.LastExecutedJobID = j.ID()
+
+		if j.Manual {
+			state.LastManualExecution = startedAt
+			state.LastManualJobID = j.ID()
+		}
+
 		switch j.State {
 		case Errored:
 			state.LastFailure = startedAt
@@ -237,13 +247,6 @@ func GetTriggerState(db prefixer.Prefixer, triggerID string) (*TriggerState, err
 			// skip any job that is not done or errored
 			continue
 		}
-		if j.Manual {
-			state.LastManualExecution = startedAt
-			state.LastManualJobID = j.ID()
-		}
-		state.LastExecution = startedAt
-		state.LastExecutedJobID = j.ID()
-		state.Status = j.State
 	}
 
 	return &state, nil
