da8094b4a711e1dadbe312c94bf126cb164a97cd
Bruno Michel <bmichel@menfin.info>
2019-04-17 17:05:15 +0200

    Revert "WIP: Temporary fix to avoid circular import for tests"
    This reverts commit 3625466a71abd5d48b614ad12459f947e86dba77.

--- web/errors/errors.go
+++ web/errors/errors.go
@@ -12,6 +12,7 @@ import (
 	"github.com/cozy/cozy-stack/pkg/instance"
 	"github.com/cozy/cozy-stack/pkg/logger"
 	"github.com/cozy/cozy-stack/web/jsonapi"
+	"github.com/cozy/cozy-stack/web/middlewares"
 
 	"github.com/cozy/echo"
 	"github.com/sirupsen/logrus"
@@ -48,7 +49,7 @@ func ErrorHandler(err error, c echo.Context) {
 
 	if config.IsDevRelease() {
 		var log *logrus.Entry
-		inst, ok := getInstanceSafe(c)
+		inst, ok := middlewares.GetInstanceSafe(c)
 		if ok {
 			log = inst.Logger().WithField("nspace", "http")
 		} else {
@@ -82,7 +83,7 @@ func HTMLErrorHandler(err error, c echo.Context) {
 	req := c.Request()
 
 	var log *logrus.Entry
-	inst, ok := getInstanceSafe(c)
+	inst, ok := middlewares.GetInstanceSafe(c)
 	if ok {
 		log = inst.Logger().WithField("nspace", "http")
 	} else {
@@ -135,7 +136,7 @@ func HTMLErrorHandler(err error, c echo.Context) {
 	} else if acceptHTML {
 		var domain string
 		var context string
-		i, ok := getInstanceSafe(c)
+		i, ok := middlewares.GetInstanceSafe(c)
 		if ok {
 			domain = i.ContextualDomain()
 			context = i.ContextName
@@ -162,13 +163,4 @@ func HTMLErrorHandler(err error, c echo.Context) {
 	if err != nil && log != nil {
 		log.Errorf("%s %s %s", req.Method, req.URL.Path, err)
 	}
-}
-
-func getInstanceSafe(c echo.Context) (*instance.Instance, bool) {
-	i := c.Get("instance")
-	if i == nil {
-		return nil, false
-	}
-	inst, ok := i.(*instance.Instance)
-	return inst, ok
 }
