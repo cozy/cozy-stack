b5e3e9d3ba336ad40187d88b306aa3789563260e
tintinmar1995 <mart.77.ma@gmail.com>
2019-05-23 19:05:24 +0200

    Create Unmarshall method for every actor's output

--- pkg/dispers/dispers/iodispers.go
+++ pkg/dispers/dispers/iodispers.go
@@ -1,5 +1,25 @@
 package dispers
 
+import "encoding/json"
+
+type Query struct {
+	Query string `json:"query,omitempty"`
+	OAuth string `json:"oauth,omitempty"`
+}
+
+type OutputCI struct {
+}
+
+func (o *OutputCI) UnmarshalJSON(data []byte) error {
+	var v [2]float64
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	//o.Price = v[0]
+	//o.Volume = v[1]
+	return nil
+}
+
 type Adresses struct {
 	ListOfAdresses []string `json:"adresses,omitempty"`
 }
@@ -8,18 +28,49 @@ type InputTF struct {
 	ListOfConcepts []Adresses `json:"concepts,omitempty"`
 }
 
+type OutputTF struct {
+}
+
+func (o *OutputTF) UnmarshalJSON(data []byte) error {
+	var v [2]float64
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	//o.Price = v[0]
+	//o.Volume = v[1]
+	return nil
+}
+
 type InputT struct {
 	Localquery      string   `json:"localquery,omitempty"`
 	ListsOfAdresses []string `json:"adresses,omitempty"`
 }
 
-type Query struct {
-	Query string `json:"query,omitempty"`
-	OAuth string `json:"oauth,omitempty"`
-}
-
 type OutputT struct {
 	Queries []Query `json:"queries,omitempty"`
+}
+
+func (o *OutputT) UnmarshalJSON(data []byte) error {
+	var v [2]float64
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	//o.Price = v[0]
+	//o.Volume = v[1]
+	return nil
+}
+
+type OutputStack struct {
+}
+
+func (o *OutputStack) UnmarshalJSON(data []byte) error {
+	var v [2]float64
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	//o.Price = v[0]
+	//o.Volume = v[1]
+	return nil
 }
 
 /*
@@ -38,4 +89,17 @@ type DescribeData struct {
 type InputDA struct {
 	InputType DescribeData `json:"type,omitempty"`
 	InputData string       `json:"data,omitempty"`
+}
+
+type OutputDA struct {
+}
+
+func (o *OutputDA) UnmarshalJSON(data []byte) error {
+	var v [2]float64
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	//o.Price = v[0]
+	//o.Volume = v[1]
+	return nil
 }
