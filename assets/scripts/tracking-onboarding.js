39df5939c8278fac7b08fdc884189104034256a6
Maxime MENAGER <maxime@cozycloud.cc>
2019-07-12 17:05:59 +0200

    Wrap the piwik initialization in a try/catch

--- assets/scripts/tracking-onboarding.js
+++ assets/scripts/tracking-onboarding.js
@@ -1,9 +1,13 @@
 ;(function(w, d) {
-  const data = d.currentScript.dataset
-  const tracker = w.Piwik.getTracker(data.matomoUrl, data.matomoSiteId)
-  tracker.enableHeartBeatTimer()
-  tracker.setUserId(w.location.hostname)
-  tracker.setCustomDimension(data.matomoAppId, 'Onboarding')
-  tracker.setCustomUrl('/password')
-  tracker.trackPageView()
+  try {
+    const data = d.currentScript.dataset
+    const tracker = w.Piwik.getTracker(data.matomoUrl, data.matomoSiteId)
+    tracker.enableHeartBeatTimer()
+    tracker.setUserId(w.location.hostname)
+    tracker.setCustomDimension(data.matomoAppId, 'Onboarding')
+    tracker.setCustomUrl('/password')
+    tracker.trackPageView()
+  } catch {
+    // Do nothing
+  }
 })(window, document)
