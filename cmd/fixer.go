055afd72061aa9cde31ef9ba565f3f7fcfb7bc52
Maxime MENAGER <maxime@cozycloud.cc>
2019-07-30 09:21:06 +0200

    Remove unused fixers

--- cmd/fixer.go
+++ cmd/fixer.go
@@ -7,18 +7,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"net/mail"
 	"net/url"
-	"os"
-	"regexp"
-	"strings"
 
 	"github.com/cozy/cozy-stack/client"
 	"github.com/cozy/cozy-stack/client/request"
 	"github.com/cozy/cozy-stack/model/account"
-	"github.com/cozy/cozy-stack/model/app"
-	"github.com/cozy/cozy-stack/model/contact"
 	"github.com/cozy/cozy-stack/model/instance/lifecycle"
 	"github.com/cozy/cozy-stack/model/job"
 	"github.com/cozy/cozy-stack/model/oauth"
@@ -35,11 +28,6 @@ var dryRunFlag bool
 var withMetadataFlag bool
 var noDryRunFlag bool
 
-var softwareIDs = map[string]string{
-	"io.cozy.drive.mobile": "registry://drive",
-	"io.cozy.banks.mobile": "registry://banks",
-}
-
 var fixerCmdGroup = &cobra.Command{
 	Use:   "fixer <command>",
 	Short: "A set of tools to fix issues or migrate content for retro-compatibility.",
@@ -55,69 +43,6 @@ var fixerCmdGroup = &cobra.Command{
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmd.Usage()
-	},
-}
-
-var albumsCreatedAtFixerCmd = &cobra.Command{
-	Use:   "albums-created-at <domain>",
-	Short: "Add a created_at field for albums where it's missing",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			return cmd.Usage()
-		}
-		c := newClient(args[0], consts.PhotosAlbums)
-		res, err := c.Req(&request.Options{
-			Method: "GET",
-			Path:   "/data/" + consts.PhotosAlbums + "/_all_docs",
-			Queries: url.Values{
-				"limit":        {"1000"},
-				"include_docs": {"true"},
-			},
-		})
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
-		var result map[string]interface{}
-		err = json.NewDecoder(res.Body).Decode(&result)
-		if err != nil {
-			return err
-		}
-		rows, ok := result["rows"].([]interface{})
-		if !ok {
-			return nil // no albums
-		}
-
-		count := 0
-		for _, r := range rows {
-			row := r.(map[string]interface{})
-			id := row["id"].(string)
-			if strings.HasPrefix(id, "_design") {
-				continue
-			}
-			album := row["doc"].(map[string]interface{})
-			if _, ok := album["created_at"]; ok {
-				continue
-			}
-			count++
-			album["created_at"] = "2017-06-01T02:03:04.000Z"
-			buf, err := json.Marshal(album)
-			if err != nil {
-				return err
-			}
-			body := bytes.NewReader(buf)
-			_, err = c.Req(&request.Options{
-				Method: "PUT",
-				Path:   "/data/" + consts.PhotosAlbums + "/" + id,
-				Body:   body,
-			})
-			if err != nil {
-				return err
-			}
-		}
-
-		fmt.Printf("Added created_at for %d albums on %s\n", count, args[0])
-		return nil
 	},
 }
 
@@ -230,40 +155,6 @@ var jobsFixer = &cobra.Command{
 	},
 }
 
-var onboardingsFixer = &cobra.Command{
-	Use:   "onboardings",
-	Short: "Add the onboarding_finished flag to user that have registered their passphrase",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		c := newAdminClient()
-		list, err := c.ListInstances()
-		if err != nil {
-			return err
-		}
-		var hasErrored bool
-		t := true
-		for _, i := range list {
-			if len(i.Attrs.RegisterToken) > 0 || i.Attrs.OnboardingFinished {
-				continue
-			}
-			fmt.Printf("Setting onboarding finished flag on '%s'...", i.Attrs.Domain)
-			_, err = c.ModifyInstance(&client.InstanceOptions{
-				Domain:             i.Attrs.Domain,
-				OnboardingFinished: &t,
-			})
-			if err != nil {
-				fmt.Printf("failed: %s\n", err)
-				hasErrored = true
-			} else {
-				fmt.Printf("ok\n")
-			}
-		}
-		if hasErrored {
-			os.Exit(1)
-		}
-		return nil
-	},
-}
-
 var redisFixer = &cobra.Command{
 	Use:   "redis",
 	Short: "Rebuild scheduling data strucutures in redis",
@@ -298,198 +189,6 @@ var thumbnailsFixer = &cobra.Command{
 			return err
 		}
 		fmt.Println(string(b))
-		return nil
-	},
-}
-
-var contactEmailsFixer = &cobra.Command{
-	Use:   "contact-emails",
-	Short: "Detect and try to fix invalid emails on contacts",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		c := newAdminClient()
-		instances, err := c.ListInstances()
-		if err != nil {
-			return err
-		}
-
-		semicolonAtEnd := regexp.MustCompile(";$")
-		dotAtStart := regexp.MustCompile(`^\.`)
-		dotAtEnd := regexp.MustCompile(`\.$`)
-		lessThanStart := regexp.MustCompile("^<")
-		greaterThanEnd := regexp.MustCompile(">$")
-		mailto := regexp.MustCompile("^mailto:")
-
-		fixEmails := func(domain string) error {
-
-			c, err := newClientSafe(domain, consts.Contacts)
-			if err != nil {
-				return err
-			}
-
-			res, err := c.Req(&request.Options{
-				Method: "GET",
-				Path:   "/data/" + consts.Contacts + "/_all_docs",
-				Queries: url.Values{
-					"include_docs": {"true"},
-				},
-			})
-			if err != nil {
-				return err
-			}
-			defer res.Body.Close()
-
-			var contacts struct {
-				Rows []struct {
-					Contact contact.Contact `json:"doc"`
-				} `json:"rows"`
-			}
-			buf, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				return err
-			}
-			err = json.Unmarshal(buf, &contacts)
-			if err != nil {
-				return err
-			}
-
-			for _, r := range contacts.Rows {
-				co := r.Contact
-				id := co.ID()
-				if strings.HasPrefix(id, "_design") {
-					continue
-				}
-
-				changed := false
-				emails, ok := co.Get("emails").([]interface{})
-				if !ok {
-					continue
-				}
-				for i := range emails {
-					email, ok := emails[i].(map[string]interface{})
-					if !ok {
-						continue
-					}
-					address, ok := email["address"].(string)
-					if !ok {
-						continue
-					}
-					_, err := mail.ParseAddress(address)
-					if err != nil {
-						old := address
-						address = strings.TrimSpace(address)
-						address = strings.Replace(address, "\"", "", -1)
-						address = strings.Replace(address, ",", ".", -1)
-						address = strings.Replace(address, " .", ".", -1)
-						address = strings.Replace(address, ". ", ".", -1)
-						address = strings.Replace(address, ". ", ".", -1)
-						address = strings.Replace(address, "..", ".", -1)
-						address = strings.Replace(address, ".@", "@", -1)
-						address = strings.Replace(address, "@.", "@", -1)
-						address = strings.Replace(address, " @", "@", -1)
-						address = strings.Replace(address, "@ ", "@", -1)
-						address = mailto.ReplaceAllString(address, "")
-						address = semicolonAtEnd.ReplaceAllString(address, "")
-						address = dotAtStart.ReplaceAllString(address, "")
-						address = dotAtEnd.ReplaceAllString(address, "")
-						address = lessThanStart.ReplaceAllString(address, "")
-						address = greaterThanEnd.ReplaceAllString(address, "")
-						address = strings.TrimSpace(address)
-						_, err := mail.ParseAddress(address)
-						if err == nil {
-							fmt.Printf("    Email fixed: \"%s\" → \"%s\"\n", old, address)
-							changed = true
-							email["address"] = address
-						} else {
-							fmt.Printf("    Invalid email: \"%s\" → \"%s\"\n", old, address)
-						}
-					}
-				}
-
-				if changed {
-					co.M["email"] = emails
-					json, err := json.Marshal(co)
-					if err != nil {
-						return err
-					}
-					body := bytes.NewReader(json)
-
-					_, err = c.Req(&request.Options{
-						Method: "PUT",
-						Path:   "/data/" + consts.Contacts + "/" + id,
-						Body:   body,
-					})
-					if err != nil {
-						return err
-					}
-				}
-			}
-
-			return nil
-		}
-
-		for _, instance := range instances {
-			domain := instance.Attrs.Domain
-			fmt.Fprintf(os.Stderr, "Fixing %s contact emails...\n", domain)
-			err := fixEmails(domain)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error occurred: %s\n", err)
-			}
-		}
-
-		return nil
-	},
-}
-
-var linkedAppFixer = &cobra.Command{
-	Use:   "link-app <domain>",
-	Short: "Link an old OAuth client to a webapp",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return cmd.Usage()
-		}
-
-		domain := args[0]
-		i, err := lifecycle.GetInstance(domain)
-		if err != nil {
-			return err
-		}
-		clients, err := oauth.GetAll(i, true)
-		if err != nil {
-			return err
-		}
-		for _, client := range clients {
-			for key, value := range softwareIDs {
-				if client.SoftwareID == key {
-					slug := oauth.GetLinkedAppSlug(value)
-
-					// Change softwareID
-					client.SoftwareID = value
-
-					// Install app
-					installer, err := app.NewInstaller(i, i.AppsCopier(consts.WebappType),
-						&app.InstallerOptions{
-							Operation:  app.Install,
-							Type:       consts.WebappType,
-							Slug:       slug,
-							SourceURL:  value,
-							Registries: i.Registries(),
-						})
-
-					if err != app.ErrAlreadyExists {
-						if err != nil {
-							return err
-						}
-						installer.Run()
-					}
-
-					err = couchdb.UpdateDoc(i, client)
-					if err != nil {
-						return err
-					}
-					break
-				}
-			}
-		}
 		return nil
 	},
 }
@@ -717,15 +416,11 @@ func init() {
 	thumbnailsFixer.Flags().BoolVar(&withMetadataFlag, "with-metadata", false, "Recalculate images metadata")
 	contentMismatch64Kfixer.Flags().BoolVar(&noDryRunFlag, "no-dry-run", false, "Do not dry run")
 
-	fixerCmdGroup.AddCommand(albumsCreatedAtFixerCmd)
 	fixerCmdGroup.AddCommand(jobsFixer)
 	fixerCmdGroup.AddCommand(md5FixerCmd)
 	fixerCmdGroup.AddCommand(mimeFixerCmd)
-	fixerCmdGroup.AddCommand(onboardingsFixer)
 	fixerCmdGroup.AddCommand(redisFixer)
 	fixerCmdGroup.AddCommand(thumbnailsFixer)
-	fixerCmdGroup.AddCommand(contactEmailsFixer)
-	fixerCmdGroup.AddCommand(linkedAppFixer)
 	fixerCmdGroup.AddCommand(contentMismatch64Kfixer)
 	fixerCmdGroup.AddCommand(orphanAccountFixer)
 
