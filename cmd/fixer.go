24ea336f6ea2646ae7c724791333ba0623af77f8
Maxime MENAGER <maxime@cozycloud.cc>
2019-07-31 10:38:03 +0200

    Re-add contactsEmailsFixer

--- cmd/fixer.go
+++ cmd/fixer.go
@@ -1,18 +1,24 @@
 package cmd
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/mail"
 	"net/url"
+	"os"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/cozy/cozy-stack/client"
 	"github.com/cozy/cozy-stack/client/request"
 	"github.com/cozy/cozy-stack/model/account"
 	"github.com/cozy/cozy-stack/model/app"
+	"github.com/cozy/cozy-stack/model/contact"
 	"github.com/cozy/cozy-stack/model/instance/lifecycle"
 	"github.com/cozy/cozy-stack/model/job"
 	"github.com/cozy/cozy-stack/model/stack"
@@ -182,6 +188,144 @@ var thumbnailsFixer = &cobra.Command{
 	},
 }
 
+var contactEmailsFixer = &cobra.Command{
+	Use:   "contact-emails",
+	Short: "Detect and try to fix invalid emails on contacts",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c := newAdminClient()
+		instances, err := c.ListInstances()
+		if err != nil {
+			return err
+		}
+
+		semicolonAtEnd := regexp.MustCompile(";$")
+		dotAtStart := regexp.MustCompile(`^\.`)
+		dotAtEnd := regexp.MustCompile(`\.$`)
+		lessThanStart := regexp.MustCompile("^<")
+		greaterThanEnd := regexp.MustCompile(">$")
+		mailto := regexp.MustCompile("^mailto:")
+
+		fixEmails := func(domain string) error {
+
+			c, err := newClientSafe(domain, consts.Contacts)
+			if err != nil {
+				return err
+			}
+
+			res, err := c.Req(&request.Options{
+				Method: "GET",
+				Path:   "/data/" + consts.Contacts + "/_all_docs",
+				Queries: url.Values{
+					"include_docs": {"true"},
+				},
+			})
+			if err != nil {
+				return err
+			}
+			defer res.Body.Close()
+
+			var contacts struct {
+				Rows []struct {
+					Contact contact.Contact `json:"doc"`
+				} `json:"rows"`
+			}
+			buf, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				return err
+			}
+			err = json.Unmarshal(buf, &contacts)
+			if err != nil {
+				return err
+			}
+
+			for _, r := range contacts.Rows {
+				co := r.Contact
+				id := co.ID()
+				if strings.HasPrefix(id, "_design") {
+					continue
+				}
+
+				changed := false
+				emails, ok := co.Get("emails").([]interface{})
+				if !ok {
+					continue
+				}
+				for i := range emails {
+					email, ok := emails[i].(map[string]interface{})
+					if !ok {
+						continue
+					}
+					address, ok := email["address"].(string)
+					if !ok {
+						continue
+					}
+					_, err := mail.ParseAddress(address)
+					if err != nil {
+						old := address
+						address = strings.TrimSpace(address)
+						address = strings.Replace(address, "\"", "", -1)
+						address = strings.Replace(address, ",", ".", -1)
+						address = strings.Replace(address, " .", ".", -1)
+						address = strings.Replace(address, ". ", ".", -1)
+						address = strings.Replace(address, ". ", ".", -1)
+						address = strings.Replace(address, "..", ".", -1)
+						address = strings.Replace(address, ".@", "@", -1)
+						address = strings.Replace(address, "@.", "@", -1)
+						address = strings.Replace(address, " @", "@", -1)
+						address = strings.Replace(address, "@ ", "@", -1)
+						address = mailto.ReplaceAllString(address, "")
+						address = semicolonAtEnd.ReplaceAllString(address, "")
+						address = dotAtStart.ReplaceAllString(address, "")
+						address = dotAtEnd.ReplaceAllString(address, "")
+						address = lessThanStart.ReplaceAllString(address, "")
+						address = greaterThanEnd.ReplaceAllString(address, "")
+						address = strings.TrimSpace(address)
+						_, err := mail.ParseAddress(address)
+						if err == nil {
+							fmt.Printf("    Email fixed: \"%s\" → \"%s\"\n", old, address)
+							changed = true
+							email["address"] = address
+						} else {
+							fmt.Printf("    Invalid email: \"%s\" → \"%s\"\n", old, address)
+						}
+					}
+				}
+
+				if changed {
+					co.M["email"] = emails
+					json, err := json.Marshal(co)
+					if err != nil {
+						return err
+					}
+					body := bytes.NewReader(json)
+
+					_, err = c.Req(&request.Options{
+						Method: "PUT",
+						Path:   "/data/" + consts.Contacts + "/" + id,
+						Body:   body,
+					})
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+			return nil
+		}
+
+		for _, instance := range instances {
+			domain := instance.Attrs.Domain
+			fmt.Fprintf(os.Stderr, "Fixing %s contact emails...\n", domain)
+			err := fixEmails(domain)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error occurred: %s\n", err)
+			}
+		}
+
+		return nil
+	},
+}
+
 var contentMismatch64Kfixer = &cobra.Command{
 	Use:   "content-mismatch <domain>",
 	Short: "Fix the content mismatch differences for 64K issue",
@@ -333,6 +477,7 @@ func init() {
 	fixerCmdGroup.AddCommand(mimeFixerCmd)
 	fixerCmdGroup.AddCommand(redisFixer)
 	fixerCmdGroup.AddCommand(thumbnailsFixer)
+	fixerCmdGroup.AddCommand(contactEmailsFixer)
 	fixerCmdGroup.AddCommand(contentMismatch64Kfixer)
 	fixerCmdGroup.AddCommand(orphanAccountFixer)
 
