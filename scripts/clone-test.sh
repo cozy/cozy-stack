44b40c0c62d7e60cac2cc3172caea102a57b3ad7
Bruno Michel <bmichel@menfin.info>
2019-10-01 12:07:25 +0200

    Remove old scripts for tests

--- scripts/clone-test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-dir=./tests/clone/generated
-mkdir -p "$dir"
-go run tests/clone/generate_tests.go $(go list -f '{{.Dir}}' ./model/...) > "$dir/clone_test.go"
-go test "$dir"
