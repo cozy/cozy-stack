0efae3bf91fc16c902369ab090ac4415e5601d32
Bruno Michel <bmichel@menfin.info>
2019-11-07 09:14:55 +0100

    Fix sharing with Swift layout v3

--- model/sharing/indexer.go
+++ model/sharing/indexer.go
@@ -223,6 +223,9 @@ func (s *sharingIndexer) bulkForceUpdateDoc(doc *vfs.FileDoc) error {
 	if doc.CozyMetadata != nil {
 		docs[0]["cozyMetadata"] = doc.CozyMetadata
 	}
+	if doc.InternalID != "" {
+		docs[0]["internalID"] = doc.InternalID
+	}
 	doc.SetRev(s.bulkRevs.Rev)
 	docs[0]["_rev"] = s.bulkRevs.Rev
 	docs[0]["_revisions"] = s.bulkRevs.Revisions
