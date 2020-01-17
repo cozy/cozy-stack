57d43cba71b501a767047d893efc8325ebf35e45
Gara64 <paul@cozycloud.cc>
2020-01-17 12:36:09 +0100

    Fix pre-login tests

--- web/bitwarden/bitwarden_test.go
+++ web/bitwarden/bitwarden_test.go
@@ -11,7 +11,6 @@ import (
 	"testing"
 
 	"github.com/cozy/cozy-stack/model/bitwarden"
-	"github.com/cozy/cozy-stack/model/bitwarden/settings"
 	"github.com/cozy/cozy-stack/model/instance"
 	"github.com/cozy/cozy-stack/model/instance/lifecycle"
 	"github.com/cozy/cozy-stack/pkg/config/config"
@@ -41,10 +40,6 @@ func TestPrelogin(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, result["Kdf"])
 	assert.Equal(t, crypto.DefaultPBKDF2Iterations, result["KdfIterations"])
-
-	settings, err := settings.Get(inst)
-	assert.NoError(t, err)
-	assert.Equal(t, settings.ExtensionInstalled, true)
 }
 
 func TestConnect(t *testing.T) {
