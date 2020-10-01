3d7751c24eb370e1eb686c6b29f1408e7fd096bb
Bruno Michel <bmichel@menfin.info>
2020-10-01 15:16:53 +0200

    Use GitHub Action to release binaries

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,12 +7,3 @@ docker build -t "cozy/cozy-app-dev:${RELEASE}" -f scripts/Dockerfile .
 docker push "cozy/cozy-app-dev:${RELEASE}"
 docker tag "cozy/cozy-app-dev:${RELEASE}" "cozy/cozy-app-dev:latest"
 docker push "cozy/cozy-app-dev:latest"
-
-GOOS=linux   GOARCH=amd64 ./scripts/build.sh release
-GOOS=linux   GOARCH=arm   ./scripts/build.sh release
-GOOS=freebsd GOARCH=amd64 ./scripts/build.sh release
-
-rm -f "*.sha256"
-
-sha256sum cozy-stack-*-${RELEASE} > "cozy-stack-${RELEASE}.sha256"
-gpg --batch --yes --detach-sign -u 0x51F72B6A45D40BBE "cozy-stack-${RELEASE}.sha256"
