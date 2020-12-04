64a0faa79c865daa62cd02c56d0a5ca4fcaaa28d
Bruno Michel <bmichel@menfin.info>
2020-12-04 17:57:03 +0100

    Improve a bit the errors for notifications (#2792)

--- model/notification/center/errors.go
+++ model/notification/center/errors.go
@@ -7,6 +7,8 @@ var (
 	ErrBadNotification = errors.New("Notification is not valid")
 	// ErrUnauthorized is used when the notification creator is not authorized to do so.
 	ErrUnauthorized = errors.New("Not authorized to send notification")
+	// ErrNoCategory is used when no category is declared for this application
+	ErrNoCategory = errors.New("No category for this app")
 	// ErrCategoryNotFound is used when sending a notification from an unknown
 	// category.
 	ErrCategoryNotFound = errors.New("Notification category does not exist")
