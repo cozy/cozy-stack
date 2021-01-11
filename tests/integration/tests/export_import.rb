fafb58d07aa47f6dcaa696db625645864174ebdb
Bruno Michel <bmichel@menfin.info>
2021-01-11 09:10:44 +0100

    Refactor the integration test for moving a Cozy (#2840)

--- tests/integration/tests/export_import.rb
+++ tests/integration/tests/export_import.rb
@@ -8,7 +8,7 @@ describe "Export and import" do
     Helpers.start_mailhog
 
     source = Instance.create name: "source"
-    dest = Instance.create name: "dest"
+    target = Instance.create name: "target"
     source.install_app "photos"
 
     # Create a file with an old version
@@ -30,46 +30,46 @@ describe "Export and import" do
     export = Export.new(source)
     export.run
     link = export.get_link
-    import = Import.new(dest, link)
+    import = Import.new(target, link)
     import.precheck
     import.run
     import.wait_done
 
-    dest.stack.reset_tokens
+    target.stack.reset_tokens
 
     # Check that everything has been moved
-    moved = Album.find dest, album.couch_id
+    moved = Album.find target, album.couch_id
     assert_equal moved.name, album.name
-    triggers = Trigger.all dest
+    triggers = Trigger.all target
     refute_nil(triggers.detect do |t|
       t.attributes.dig("message", "name") == "onPhotoUpload"
     end) # It's a service for the photos app
 
-    dir = Folder.find dest, folder.couch_id
+    dir = Folder.find target, folder.couch_id
     %i[couch_id name dir_id path].each do |prop|
       assert_equal dir.send(prop), folder.send(prop)
     end
     photos.each do |p|
-      photo = CozyFile.find dest, p.couch_id
+      photo = CozyFile.find target, p.couch_id
       %i[couch_id name dir_id mime].each do |prop|
         assert_equal photo.send(prop), p.send(prop)
       end
     end
-    f = CozyFile.find dest, file.couch_id
+    f = CozyFile.find target, file.couch_id
     assert_equal f.old_versions.length, 1
 
-    # Check that the email from the destination was kept
-    contacts = Contact.all dest
+    # Check that the email address from the target was kept
+    contacts = Contact.all target
     me = contacts.detect(&:me)
-    assert_equal me.emails[0]["address"], dest.email
-    settings = Settings.instance dest
-    assert_equal settings["email"], dest.email
+    assert_equal me.emails[0]["address"], target.email
+    settings = Settings.instance target
+    assert_equal settings["email"], target.email
 
     # It is the end
     assert_equal source.check, []
-    assert_equal dest.check, []
+    assert_equal target.check, []
 
     source.remove
-    dest.remove
+    target.remove
   end
 end
