40aad9f541743122738639e5be8bfb0f1c50e405
Quentin Valmori <quentin.valmori@gmail.com>
2022-05-12 09:05:48 +0200

    fix: Don't use the UserID feature of matomo (#3397)

--- assets/scripts/tracking-onboarding.js
+++ assets/scripts/tracking-onboarding.js
@@ -3,7 +3,6 @@
     const data = d.currentScript.dataset
     const tracker = w.Piwik.getTracker(data.matomoUrl, data.matomoSiteId)
     tracker.enableHeartBeatTimer()
-    tracker.setUserId(w.location.hostname)
     tracker.setCustomDimension(data.matomoAppId, 'Onboarding')
     tracker.setCustomUrl('/password')
     tracker.trackPageView()
