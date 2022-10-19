1c80550255597803e6e1c24493bf3470b7a31bf3
Bruno Michel <bmichel@menfin.info>
2022-10-19 16:36:13 +0200

    Add log for Operation aborted (#3537)

--- model/sharing/error.go
+++ model/sharing/error.go
@@ -44,7 +44,7 @@ var (
 	// ErrFolderNotFound is used when informations about a folder is asked,
 	// but this folder was not found
 	ErrFolderNotFound = errors.New("This folder was not found")
-	// ErrSafety is used when an operation is aborted due to the safery principal
+	// ErrSafety is used when an operation is aborted due to the safety principal
 	ErrSafety = errors.New("Operation aborted")
 	// ErrAlreadyAccepted is used when someone tries to accept twice a sharing
 	// on the same cozy instance
