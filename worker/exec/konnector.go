f85500075fc57edc760f8c676d0437256dcf635b
Sébastien Blaisot <sebastien.blaisot@cozycloud.cc>
2022-11-20 23:01:00 +0100

    Avoid one access to older app.tar when unnecessary

--- worker/exec/konnector.go
+++ worker/exec/konnector.go
@@ -242,15 +242,7 @@ func (w *konnectorWorker) PrepareWorkDir(ctx *job.WorkerContext, i *instance.Ins
 	workFS := afero.NewBasePathFs(osFS, workDir)
 
 	fileServer := app.KonnectorsFileServer(i)
-	tarFile, err := fileServer.Open(slug, man.Version(), man.Checksum(), app.KonnectorArchiveName)
-	if err == nil {
-		err = extractTar(workFS, tarFile)
-		if errc := tarFile.Close(); err == nil {
-			err = errc
-		}
-	} else if os.IsNotExist(err) {
-		err = copyFiles(workFS, fileServer, slug, man.Version(), man.Checksum())
-	}
+	err = copyFiles(workFS, fileServer, slug, man.Version(), man.Checksum())
 	if err != nil {
 		return "", cleanDir, err
 	}
@@ -432,6 +424,21 @@ func copyFiles(workFS afero.Fs, fileServer appfs.FileServer, slug, version, shas
 			"package.json",
 			".travis.yml",
 			"LICENSE":
+			continue
+		// Backward compatibility with older konnector storage pattern
+		// in unique tar file which has ben removed in #1332
+		case app.KonnectorArchiveName:
+			tarFile, err := fileServer.Open(slug, version, shasum, file)
+			if err != nil {
+				return err
+			}
+			err = extractTar(workFS, tarFile)
+			if errc := tarFile.Close(); err == nil {
+				err = errc
+			}
+			if err != nil {
+				return err
+			}
 			continue
 		}
 		var src io.ReadCloser
