4f3389b703bc6d177fce7c859862a06c02dc1c91
Peltoche <pierre.peltier@protonmail.com>
2023-01-30 11:44:52 +0100

    Use `gavv/httpexpect` for the `/web/shortcuts` tests

--- web/shortcuts/shortcuts_test.go
+++ web/shortcuts/shortcuts_test.go
@@ -1,26 +1,14 @@
 package shortcuts
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"net/http"
-	"net/http/httptest"
 	"testing"
+	"time"
 
-	"github.com/cozy/cozy-stack/model/instance"
 	"github.com/cozy/cozy-stack/pkg/config/config"
 	"github.com/cozy/cozy-stack/pkg/consts"
 	"github.com/cozy/cozy-stack/tests/testutils"
-	weberrors "github.com/cozy/cozy-stack/web/errors"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
+	"github.com/gavv/httpexpect/v2"
 )
-
-var ts *httptest.Server
-var inst *instance.Instance
-var token string
-var shortcutID string
 
 const targetURL = "https://alice-photos.cozy.example/#/photos/629fb233be550a21174ac8e19f0043af"
 
@@ -29,100 +17,97 @@ func TestShortcuts(t *testing.T) {
 		t.Skip("an instance is required for this test: test skipped due to the use of --short flag")
 	}
 
+	var shortcutID string
+
 	config.UseTestFile()
 	testutils.NeedCouchdb(t)
 	setup := testutils.NewSetup(t, t.Name())
-	inst = setup.GetTestInstance()
-	_, token = setup.GetTestClient(consts.Files)
+	// _ = setup.GetTestInstance()
+	_, token := setup.GetTestClient(consts.Files)
 
-	ts = setup.GetTestServer("/shortcuts", Routes)
-	ts.Config.Handler.(*echo.Echo).HTTPErrorHandler = weberrors.ErrorHandler
+	ts := setup.GetTestServer("/shortcuts", Routes)
+	t.Cleanup(ts.Close)
+
+	// ts.Config.Handler.(*echo.Echo).HTTPErrorHandler = weberrors.ErrorHandler
 
 	t.Run("CreateShortcut", func(t *testing.T) {
-		body := `
-{
-  "data": {
-    "type": "io.cozy.files.shortcuts",
-    "attributes": {
-      "name": "sunset.jpg.url",
-      "url": "` + targetURL + `",
-      "metadata": {
-        "target": {
-          "cozyMetadata": {
-            "instance": "https://alice.cozy.example/"
-          },
-          "app": "photos",
-          "_type": "io.cozy.files",
-          "mime": "image/jpg"
-        }
-      }
-    }
-  }
-}`
-		req, _ := http.NewRequest("POST", ts.URL+"/shortcuts", bytes.NewBufferString(body))
-		req.Header.Add("Content-Type", "application/vnd.api+json")
-		req.Header.Add("Authorization", "Bearer "+token)
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		assert.Equal(t, 201, res.StatusCode)
-		var result map[string]interface{}
-		err = json.NewDecoder(res.Body).Decode(&result)
-		assert.NoError(t, err)
+		e := httpexpect.Default(t, ts.URL)
 
-		data, _ := result["data"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.files", data["type"])
-		assert.Contains(t, data, "id")
-		shortcutID = data["id"].(string)
-		attrs := data["attributes"].(map[string]interface{})
-		assert.Equal(t, "file", attrs["type"])
-		assert.Equal(t, "sunset.jpg.url", attrs["name"])
-		assert.Equal(t, "application/internet-shortcut", attrs["mime"])
-		fcm, _ := attrs["cozyMetadata"].(map[string]interface{})
-		assert.Contains(t, fcm, "createdAt")
-		assert.Contains(t, fcm, "createdOn")
-		meta, _ := attrs["metadata"].(map[string]interface{})
-		target, _ := meta["target"].(map[string]interface{})
-		assert.Equal(t, "photos", target["app"])
-		assert.Equal(t, "io.cozy.files", target["_type"])
-		assert.Equal(t, "image/jpg", target["mime"])
+		obj := e.POST("/shortcuts").
+			WithHeader("Content-Type", "application/vnd.api+json").
+			WithHeader("Authorization", "Bearer "+token).
+			WithBytes([]byte(`{
+        "data": {
+          "type": "io.cozy.files.shortcuts",
+          "attributes": {
+            "name": "sunset.jpg.url",
+            "url": "` + targetURL + `",
+            "metadata": {
+              "target": {
+                "cozyMetadata": {
+                  "instance": "https://alice.cozy.example/"
+                },
+                "app": "photos",
+                "_type": "io.cozy.files",
+                "mime": "image/jpg"
+              }
+            }
+          }
+        }
+      }`)).
+			Expect().Status(201).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
+
+		data := obj.Value("data").Object()
+
+		data.ValueEqual("type", "io.cozy.files")
+		shortcutID = data.Value("id").String().NotEmpty().Raw()
+
+		attrs := data.Value("attributes").Object()
+		attrs.ValueEqual("type", "file")
+		attrs.ValueEqual("name", "sunset.jpg.url")
+		attrs.ValueEqual("mime", "application/internet-shortcut")
+
+		cozyMeta := attrs.Value("cozyMetadata").Object()
+		cozyMeta.Value("createdAt").String().DateTime(time.RFC3339)
+		cozyMeta.Value("createdOn").String().Contains("https://testshortcuts_")
+
+		target := attrs.Value("metadata").Object().Value("target").Object()
+		target.ValueEqual("app", "photos")
+		target.ValueEqual("_type", "io.cozy.files")
+		target.ValueEqual("mime", "image/jpg")
 	})
 
 	t.Run("GetShortcut", func(t *testing.T) {
-		req, _ := http.NewRequest("GET", ts.URL+"/shortcuts/"+shortcutID, nil)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Add("Accept", "application/vnd.api+json")
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res.StatusCode)
-		var result map[string]interface{}
-		err = json.NewDecoder(res.Body).Decode(&result)
-		assert.NoError(t, err)
+		e := httpexpect.Default(t, ts.URL)
 
-		data, _ := result["data"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.files.shortcuts", data["type"])
-		assert.Equal(t, shortcutID, data["id"])
-		shortcutID = data["id"].(string)
-		attrs := data["attributes"].(map[string]interface{})
-		assert.Equal(t, "sunset.jpg.url", attrs["name"])
-		assert.Equal(t, "io.cozy.files.root-dir", attrs["dir_id"])
-		assert.Equal(t, targetURL, attrs["url"])
-		meta := attrs["metadata"].(map[string]interface{})
-		target := meta["target"].(map[string]interface{})
-		assert.Equal(t, "photos", target["app"])
-		assert.Equal(t, "io.cozy.files", target["_type"])
-		assert.Equal(t, "image/jpg", target["mime"])
+		obj := e.GET("/shortcuts/"+shortcutID).
+			WithHeader("Accept", "application/json").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
 
-		client := http.Client{
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return errors.New("Do not follow the redirections")
-			},
-		}
-		req, _ = http.NewRequest("GET", ts.URL+"/shortcuts/"+shortcutID, nil)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Add("Accept", "text/html")
-		res, err = client.Do(req)
-		assert.Contains(t, err.Error(), "Do not follow the redirections")
-		assert.Equal(t, 303, res.StatusCode)
-		assert.Equal(t, targetURL, res.Header.Get("Location"))
+		data := obj.Value("data").Object()
+		data.ValueEqual("type", "io.cozy.files.shortcuts")
+		data.ValueEqual("id", shortcutID)
+
+		attrs := data.Value("attributes").Object()
+		attrs.ValueEqual("name", "sunset.jpg.url")
+		attrs.ValueEqual("dir_id", "io.cozy.files.root-dir")
+		attrs.ValueEqual("url", targetURL)
+
+		target := attrs.Value("metadata").Object().Value("target").Object()
+		target.ValueEqual("app", "photos")
+		target.ValueEqual("_type", "io.cozy.files")
+		target.ValueEqual("mime", "image/jpg")
+
+		e.GET("/shortcuts/"+shortcutID).
+			WithRedirectPolicy(httpexpect.DontFollowRedirects).
+			WithHeader("Authorization", "Bearer "+token).
+			WithHeader("Accept", "text/html").
+			Expect().Status(303).
+			Header("Location").Equal(targetURL)
 	})
 }
