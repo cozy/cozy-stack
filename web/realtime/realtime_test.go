ca99ff7ebdbe6224e42f16509c58aa8edf1f7670
Peltoche <pierre.peltier@protonmail.com>
2023-02-01 10:10:48 +0100

    Use `gavv/httpexpect` for the `web/realtime` tests

--- web/realtime/realtime_test.go
+++ web/realtime/realtime_test.go
@@ -1,26 +1,16 @@
 package realtime
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
-	"net/http/httptest"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/cozy/cozy-stack/model/instance"
 	"github.com/cozy/cozy-stack/pkg/config/config"
 	"github.com/cozy/cozy-stack/pkg/realtime"
 	"github.com/cozy/cozy-stack/tests/testutils"
-	"github.com/gorilla/websocket"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/gavv/httpexpect/v2"
 )
-
-var ts *httptest.Server
-var inst *instance.Instance
-var token string
 
 type testDoc struct {
 	id      string
@@ -35,122 +25,112 @@ func TestRealtime(t *testing.T) {
 	config.UseTestFile()
 	testutils.NeedCouchdb(t)
 	setup := testutils.NewSetup(t, t.Name())
-	inst = setup.GetTestInstance()
-	_, token = setup.GetTestClient("io.cozy.foos io.cozy.bars io.cozy.bazs")
-	ts = setup.GetTestServer("/realtime", Routes)
+	inst := setup.GetTestInstance()
+	_, token := setup.GetTestClient("io.cozy.foos io.cozy.bars io.cozy.bazs")
+	ts := setup.GetTestServer("/realtime", Routes)
+	t.Cleanup(ts.Close)
 
 	t.Run("WSNoAuth", func(t *testing.T) {
-		u := strings.Replace(ts.URL+"/realtime/", "http", "ws", 1)
-		ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-		assert.NoError(t, err)
-		defer ws.Close()
+		e := httpexpect.Default(t, ts.URL)
 
-		msg := `{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.foos" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		assert.NoError(t, err)
+		ws := e.GET("/realtime/").
+			WithWebsocketUpgrade().
+			Expect().Status(http.StatusSwitchingProtocols).
+			Websocket()
+		defer ws.Disconnect()
 
-		var res map[string]interface{}
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "error", res["event"])
-		payload := res["payload"].(map[string]interface{})
-		assert.Equal(t, "405 Method Not Allowed", payload["status"])
-		assert.Equal(t, "method not allowed", payload["code"])
-		assert.Equal(t, "The SUBSCRIBE method is not supported", payload["title"])
+		obj := ws.WriteText(`{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.foos" }}`).
+			Expect().TextMessage().
+			JSON().Object()
+
+		obj.ValueEqual("event", "error")
+		payload := obj.Value("payload").Object()
+		payload.ValueEqual("status", "405 Method Not Allowed")
+		payload.ValueEqual("code", "method not allowed")
+		payload.ValueEqual("title", "The SUBSCRIBE method is not supported")
 	})
 
 	t.Run("WSInvalidToken", func(t *testing.T) {
-		u := strings.Replace(ts.URL+"/realtime/", "http", "ws", 1)
-		ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-		assert.NoError(t, err)
-		defer ws.Close()
+		e := httpexpect.Default(t, ts.URL)
 
-		auth := `{"method": "AUTH", "payload": "123456789"}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(auth))
-		assert.NoError(t, err)
+		ws := e.GET("/realtime/").
+			WithWebsocketUpgrade().
+			Expect().Status(http.StatusSwitchingProtocols).
+			Websocket()
+		defer ws.Disconnect()
 
-		var res map[string]interface{}
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "error", res["event"])
-		payload := res["payload"].(map[string]interface{})
-		assert.Equal(t, "401 Unauthorized", payload["status"])
-		assert.Equal(t, "unauthorized", payload["code"])
-		assert.Equal(t, "The authentication has failed", payload["title"])
+		obj := ws.WriteText(`{"method": "AUTH", "payload": "123456789"}`).
+			Expect().TextMessage().
+			JSON().Object()
+
+		obj.ValueEqual("event", "error")
+		payload := obj.Value("payload").Object()
+		payload.ValueEqual("status", "401 Unauthorized")
+		payload.ValueEqual("code", "unauthorized")
+		payload.ValueEqual("title", "The authentication has failed")
 	})
 
 	t.Run("WSNoPermissionsForADoctype", func(t *testing.T) {
-		u := strings.Replace(ts.URL+"/realtime/", "http", "ws", 1)
-		ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-		assert.NoError(t, err)
-		defer ws.Close()
+		e := httpexpect.Default(t, ts.URL)
 
-		auth := fmt.Sprintf(`{"method": "AUTH", "payload": "%s"}`, token)
-		err = ws.WriteMessage(websocket.TextMessage, []byte(auth))
-		assert.NoError(t, err)
+		ws := e.GET("/realtime/").
+			WithWebsocketUpgrade().
+			Expect().Status(http.StatusSwitchingProtocols).
+			Websocket()
+		defer ws.Disconnect()
 
-		msg := `{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.contacts" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		assert.NoError(t, err)
+		ws.WriteText(fmt.Sprintf(`{"method": "AUTH", "payload": "%s"}`, token))
 
-		var res map[string]interface{}
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "error", res["event"])
-		payload := res["payload"].(map[string]interface{})
-		assert.Equal(t, "403 Forbidden", payload["status"])
-		assert.Equal(t, "forbidden", payload["code"])
-		assert.Equal(t, "The application can't subscribe to io.cozy.contacts", payload["title"])
+		obj := ws.WriteText(`{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.contacts" }}`).
+			Expect().TextMessage().
+			JSON().Object()
+
+		obj.ValueEqual("event", "error")
+		payload := obj.Value("payload").Object()
+		payload.ValueEqual("status", "403 Forbidden")
+		payload.ValueEqual("code", "forbidden")
+		payload.ValueEqual("title", "The application can't subscribe to io.cozy.contacts")
 	})
 
 	t.Run("WSSuccess", func(t *testing.T) {
-		u := strings.Replace(ts.URL+"/realtime/", "http", "ws", 1)
-		ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-		require.NoError(t, err)
+		e := httpexpect.Default(t, ts.URL)
 
-		defer ws.Close()
+		ws := e.GET("/realtime/").
+			WithWebsocketUpgrade().
+			Expect().Status(http.StatusSwitchingProtocols).
+			Websocket()
+		defer ws.Disconnect()
 
-		auth := fmt.Sprintf(`{"method": "AUTH", "payload": "%s"}`, token)
-		err = ws.WriteMessage(websocket.TextMessage, []byte(auth))
-		require.NoError(t, err)
+		ws.WriteText(fmt.Sprintf(`{"method": "AUTH", "payload": "%s"}`, token))
 
-		msg := `{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.foos" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		require.NoError(t, err)
-
-		msg = `{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.bars", "id": "bar-one" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		require.NoError(t, err)
-
-		msg = `{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.bars", "id": "bar-two" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		require.NoError(t, err)
+		ws.WriteText(`{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.foos" }}`)
+		ws.WriteText(`{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.bars", "id": "bar-one"  }}`)
+		ws.WriteText(`{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.bars", "id": "bar-two" }}`)
 
 		h := realtime.GetHub()
-		var res map[string]interface{}
 		time.Sleep(30 * time.Millisecond)
 
 		h.Publish(inst, realtime.EventUpdate, &testDoc{
 			doctype: "io.cozy.foos",
 			id:      "foo-one",
 		}, nil)
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "UPDATED", res["event"])
-		payload := res["payload"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.foos", payload["type"])
-		assert.Equal(t, "foo-one", payload["id"])
+
+		obj := ws.Expect().TextMessage().JSON().Object()
+		obj.ValueEqual("event", "UPDATED")
+		payload := obj.Value("payload").Object()
+		payload.ValueEqual("type", "io.cozy.foos")
+		payload.ValueEqual("id", "foo-one")
 
 		h.Publish(inst, realtime.EventDelete, &testDoc{
 			doctype: "io.cozy.foos",
 			id:      "foo-two",
 		}, nil)
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "DELETED", res["event"])
-		payload = res["payload"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.foos", payload["type"])
-		assert.Equal(t, "foo-two", payload["id"])
+
+		obj = ws.Expect().TextMessage().JSON().Object()
+		obj.ValueEqual("event", "DELETED")
+		payload = obj.Value("payload").Object()
+		payload.ValueEqual("type", "io.cozy.foos")
+		payload.ValueEqual("id", "foo-two")
 
 		h.Publish(inst, realtime.EventCreate, &testDoc{
 			doctype: "io.cozy.bars",
@@ -162,17 +142,14 @@ func TestRealtime(t *testing.T) {
 			doctype: "io.cozy.bars",
 			id:      "bar-one",
 		}, nil)
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "CREATED", res["event"])
-		payload = res["payload"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.bars", payload["type"])
-		assert.Equal(t, "bar-one", payload["id"])
 
-		msg = `{"method": "UNSUBSCRIBE", "payload": { "type": "io.cozy.bars", "id": "bar-one" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		require.NoError(t, err)
+		obj = ws.Expect().TextMessage().JSON().Object()
+		obj.ValueEqual("event", "CREATED")
+		payload = obj.Value("payload").Object()
+		payload.ValueEqual("type", "io.cozy.bars")
+		payload.ValueEqual("id", "bar-one")
 
+		ws.WriteText(`{"method": "UNSUBSCRIBE", "payload": { "type": "io.cozy.bars", "id": "bar-one" }}`)
 		time.Sleep(30 * time.Millisecond)
 
 		h.Publish(inst, realtime.EventUpdate, &testDoc{
@@ -185,48 +162,42 @@ func TestRealtime(t *testing.T) {
 			doctype: "io.cozy.bars",
 			id:      "bar-two",
 		}, nil)
-		err = ws.ReadJSON(&res)
-		assert.NoError(t, err)
-		assert.Equal(t, "UPDATED", res["event"])
-		payload = res["payload"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.bars", payload["type"])
-		assert.Equal(t, "bar-two", payload["id"])
+
+		obj = ws.Expect().TextMessage().JSON().Object()
+		obj.ValueEqual("event", "UPDATED")
+		payload = obj.Value("payload").Object()
+		payload.ValueEqual("type", "io.cozy.bars")
+		payload.ValueEqual("id", "bar-two")
 	})
 
 	t.Run("WSNotify", func(t *testing.T) {
-		u := strings.Replace(ts.URL+"/realtime/", "http", "ws", 1)
-		ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-		require.NoError(t, err)
+		e := httpexpect.Default(t, ts.URL)
 
-		defer ws.Close()
+		ws := e.GET("/realtime/").
+			WithWebsocketUpgrade().
+			Expect().Status(http.StatusSwitchingProtocols).
+			Websocket()
+		defer ws.Disconnect()
 
-		auth := fmt.Sprintf(`{"method": "AUTH", "payload": "%s"}`, token)
-		err = ws.WriteMessage(websocket.TextMessage, []byte(auth))
-		require.NoError(t, err)
+		ws.WriteText(fmt.Sprintf(`{"method": "AUTH", "payload": "%s"}`, token))
 
-		msg := `{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.bazs", "id": "baz-one" }}`
-		err = ws.WriteMessage(websocket.TextMessage, []byte(msg))
-		require.NoError(t, err)
-
+		ws.WriteText(`{"method": "SUBSCRIBE", "payload": { "type": "io.cozy.bazs", "id": "baz-one"  }}`)
 		time.Sleep(30 * time.Millisecond)
-		body := `{"hello": "world"}`
-		req, _ := http.NewRequest("POST", ts.URL+"/realtime/io.cozy.bazs/baz-one", bytes.NewBufferString(body))
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Authorization", "Bearer "+token)
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		require.Equal(t, http.StatusNoContent, res.StatusCode)
 
-		var resp map[string]interface{}
-		err = ws.ReadJSON(&resp)
-		require.NoError(t, err)
+		e.POST("/realtime/io.cozy.bazs/baz-one").
+			WithHeader("Content-Type", "application/json").
+			WithHeader("Authorization", "Bearer "+token).
+			WithBytes([]byte(`{ "hello": "world" }`)).
+			Expect().Status(204)
 
-		assert.Equal(t, "NOTIFIED", resp["event"])
-		payload := resp["payload"].(map[string]interface{})
-		assert.Equal(t, "io.cozy.bazs", payload["type"])
-		assert.Equal(t, "baz-one", payload["id"])
-		doc := payload["doc"].(map[string]interface{})
-		assert.Equal(t, "world", doc["hello"])
+		obj := ws.Expect().TextMessage().JSON().Object()
+		obj.ValueEqual("event", "NOTIFIED")
+		payload := obj.Value("payload").Object()
+		payload.ValueEqual("type", "io.cozy.bazs")
+		payload.ValueEqual("id", "baz-one")
+
+		doc := payload.Value("doc").Object()
+		doc.ValueEqual("hello", "world")
 	})
 }
 
