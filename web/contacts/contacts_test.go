653edd36b57133e5e39c33a610369b747f664dc6
Peltoche <pierre.peltier@protonmail.com>
2023-02-13 19:38:45 +0100

    Use `gavv/httpexpect` for the `/web/contacts` tests

--- web/contacts/contacts_test.go
+++ web/contacts/contacts_test.go
@@ -1,24 +1,17 @@
 package contacts
 
 import (
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/cozy/cozy-stack/model/contact"
-	"github.com/cozy/cozy-stack/model/instance"
 	"github.com/cozy/cozy-stack/model/instance/lifecycle"
 	"github.com/cozy/cozy-stack/pkg/config/config"
 	"github.com/cozy/cozy-stack/pkg/consts"
 	"github.com/cozy/cozy-stack/pkg/couchdb"
 	"github.com/cozy/cozy-stack/tests/testutils"
+	"github.com/gavv/httpexpect/v2"
 	"github.com/stretchr/testify/assert"
 )
-
-var ts *httptest.Server
-var testInstance *instance.Instance
-var token string
 
 func TestContacts(t *testing.T) {
 	if testing.Short() {
@@ -28,48 +21,51 @@ func TestContacts(t *testing.T) {
 	config.UseTestFile()
 	testutils.NeedCouchdb(t)
 	setup := testutils.NewSetup(t, t.Name())
-	testInstance = setup.GetTestInstance(&lifecycle.Options{
+	testInstance := setup.GetTestInstance(&lifecycle.Options{
 		Email:      "alice@example.com",
 		PublicName: "Alice",
 	})
-	_, token = setup.GetTestClient(consts.Contacts)
-	ts = setup.GetTestServer("/contacts", Routes)
+	_, token := setup.GetTestClient(consts.Contacts)
+	ts := setup.GetTestServer("/contacts", Routes)
+	t.Cleanup(ts.Close)
 
 	t.Run("Myself", func(t *testing.T) {
-		req, _ := http.NewRequest("POST", ts.URL+"/contacts/myself", nil)
-		req.Header.Add("Authorization", "Bearer "+token)
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		assertMyself(t, res)
+		e := testutils.CreateTestClient(t, ts.URL)
 
+		// Create an empty contact for myself
+		e.POST("/contacts/myself").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200)
+
+		// Get the contact info about myself
 		myself, err := contact.GetMyself(testInstance)
 		assert.NoError(t, err)
+
+		// Delete the contacts info about myself
 		err = couchdb.DeleteDoc(testInstance, myself)
 		assert.NoError(t, err)
-		res2, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		assertMyself(t, res2)
+
+		// Create again an empty contact for myself ?
+		obj := e.POST("/contacts/myself").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
+
+		// Check the contact data
+		data := obj.Value("data").Object()
+		data.Value("id").String().NotEmpty()
+		data.ValueEqual("type", consts.Contacts)
+
+		meta := data.Value("meta").Object()
+		meta.Value("rev").String().NotEmpty()
+
+		attrs := data.Value("attributes").Object()
+		attrs.ValueEqual("fullname", "Alice")
+		emails := attrs.Value("email").Array()
+		emails.Length().Equal(1)
+		email := emails.First().Object()
+		email.ValueEqual("address", "alice@example.com")
+		email.ValueEqual("primary", true)
 	})
-}
-
-func assertMyself(t *testing.T, res *http.Response) {
-	assert.Equal(t, 200, res.StatusCode)
-	var result map[string]interface{}
-	err := json.NewDecoder(res.Body).Decode(&result)
-	assert.NoError(t, err)
-
-	data, _ := result["data"].(map[string]interface{})
-	assert.NotEmpty(t, data["id"])
-	assert.Equal(t, consts.Contacts, data["type"])
-	meta, _ := data["meta"].(map[string]interface{})
-	assert.NotEmpty(t, meta["rev"])
-
-	attrs, _ := data["attributes"].(map[string]interface{})
-	assert.Equal(t, "Alice", attrs["fullname"])
-	emails, _ := attrs["email"].([]interface{})
-	if assert.Len(t, emails, 1) {
-		email, _ := emails[0].(map[string]interface{})
-		assert.Equal(t, "alice@example.com", email["address"])
-		assert.Equal(t, true, email["primary"])
-	}
 }
