984cc70202dda816d13834385fab6d537a691efd
Peltoche <pierre.peltier@protonmail.com>
2023-02-16 10:35:02 +0100

    Use `gavv/httpexpect` for the `/web/data` tests

--- web/data/references_test.go
+++ web/data/references_test.go
@@ -1,14 +1,17 @@
 package data
 
 import (
-	"net/http"
+	"net/url"
 	"testing"
 	"time"
 
+	"github.com/cozy/cozy-stack/model/instance"
 	"github.com/cozy/cozy-stack/model/vfs"
+	"github.com/cozy/cozy-stack/pkg/config/config"
 	"github.com/cozy/cozy-stack/pkg/consts"
 	"github.com/cozy/cozy-stack/pkg/couchdb"
-	"github.com/cozy/cozy-stack/pkg/jsonapi"
+	"github.com/cozy/cozy-stack/tests/testutils"
+	"github.com/gavv/httpexpect/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -18,79 +21,95 @@ func TestReferences(t *testing.T) {
 		t.Skip("an instance is required for this test: test skipped due to the use of --short flag")
 	}
 
+	const Type = "io.cozy.events"
+	const ID = "4521C325F6478E45"
+
+	config.UseTestFile()
+	testutils.NeedCouchdb(t)
+	setup := testutils.NewSetup(t, t.Name())
+	testInstance := setup.GetTestInstance()
+	scope := "io.cozy.doctypes io.cozy.files io.cozy.events " +
+		"io.cozy.anothertype io.cozy.nottype"
+
+	_, token := setup.GetTestClient(scope)
+	ts := setup.GetTestServer("/data", Routes)
+	t.Cleanup(ts.Close)
+
+	_ = couchdb.ResetDB(testInstance, Type)
+	_ = couchdb.CreateNamedDoc(testInstance, &couchdb.JSONDoc{
+		Type: Type,
+		M: map[string]interface{}{
+			"_id":  ID,
+			"test": "testvalue",
+		},
+	})
+
 	t.Run("ListNotSynchronizedOn", func(t *testing.T) {
+		e := testutils.CreateTestClient(t, ts.URL)
+
 		// Make doc
-		doc := getDocForTest()
-		url := ts.URL + "/data/" + doc.DocType() + "/" + doc.ID() + "/relationships/references"
+		doc := getDocForTest(Type, testInstance)
 
 		// Make Files
-		makeReferencedTestFile(t, doc, "testtoref2.txt")
-		makeReferencedTestFile(t, doc, "testtoref3.txt")
-		makeReferencedTestFile(t, doc, "testtoref4.txt")
-		makeReferencedTestFile(t, doc, "testtoref5.txt")
+		makeReferencedTestFile(t, testInstance, doc, "testtoref2.txt")
+		makeReferencedTestFile(t, testInstance, doc, "testtoref3.txt")
+		makeReferencedTestFile(t, testInstance, doc, "testtoref4.txt")
+		makeReferencedTestFile(t, testInstance, doc, "testtoref5.txt")
 
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Add("Authorization", "Bearer "+token)
+		// Simple query
+		obj := e.GET("/data/"+doc.DocType()+"/"+doc.ID()+"/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
 
-		var result struct {
-			Links jsonapi.LinksList
-			Data  []couchdb.DocReference `json:"data"`
-			Meta  jsonapi.Meta
-		}
-		_, res, err := doRequest(req, &result)
+		obj.Path("$.meta.count").Equal(4)
+		obj.Value("data").Array().Length().Equal(4)
+		obj.Value("links").Object().NotContainsKey("next")
 
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res.StatusCode)
-		assert.Len(t, result.Data, 4)
-		assert.Equal(t, *result.Meta.Count, 4)
-		assert.Empty(t, result.Links.Next)
+		// Use the page limit
+		obj = e.GET("/data/"+doc.DocType()+"/"+doc.ID()+"/relationships/references").
+			WithQuery("page[limit]", 3).
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
 
-		var result2 struct {
-			Links jsonapi.LinksList
-			Data  []couchdb.DocReference `json:"data"`
-		}
-		req2, _ := http.NewRequest("GET", url+"?page[limit]=3", nil)
-		req2.Header.Add("Authorization", "Bearer "+token)
-		_, res2, err := doRequest(req2, &result2)
+		obj.Path("$.meta.count").Equal(4)
+		obj.Value("data").Array().Length().Equal(3)
+		rawNext := obj.Value("links").Object().Value("next").String().NotEmpty().Raw()
 
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res2.StatusCode)
-		assert.Len(t, result2.Data, 3)
-		assert.Equal(t, *result.Meta.Count, 4)
-		assert.NotEmpty(t, result2.Links.Next)
+		nextURL, err := url.Parse(rawNext)
+		require.NoError(t, err)
 
-		var result3 struct {
-			Links jsonapi.LinksList
-			Data  []couchdb.DocReference `json:"data"`
-		}
-		req3, _ := http.NewRequest("GET", ts.URL+result2.Links.Next, nil)
-		req3.Header.Add("Authorization", "Bearer "+token)
-		_, res3, err := doRequest(req3, &result3)
+		// Use the bookmark
+		obj = e.GET(nextURL.Path).
+			WithQueryString(nextURL.RawQuery).
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
 
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res3.StatusCode)
-		assert.Len(t, result3.Data, 1)
-		assert.Empty(t, result3.Links.Next)
+		obj.Value("data").Array().Length().Equal(1)
+		obj.Value("links").Object().NotContainsKey("next")
 
-		var result4 struct {
-			Links    jsonapi.LinksList
-			Data     []couchdb.DocReference `json:"data"`
-			Included []interface{}          `json:"included"`
-		}
-		req4, _ := http.NewRequest("GET", url+"?include=files", nil)
-		req4.Header.Add("Authorization", "Bearer "+token)
-		_, res4, err := doRequest(req4, &result4)
+		// Include the files
+		obj = e.GET("/data/"+doc.DocType()+"/"+doc.ID()+"/relationships/references").
+			WithQuery("include", "files").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
 
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res4.StatusCode)
-		assert.Len(t, result4.Included, 4)
-		assert.NotEmpty(t, result4.Included[0].(map[string]interface{})["id"])
+		obj.Value("included").Array().Length().Equal(4)
+		obj.Path("$.included[0].id").String().NotEmpty()
 	})
 
 	t.Run("AddReferencesHandler", func(t *testing.T) {
+		e := testutils.CreateTestClient(t, ts.URL)
+
 		// Make doc
-		doc := getDocForTest()
-		url := ts.URL + "/data/" + doc.DocType() + "/" + doc.ID() + "/relationships/references"
+		doc := getDocForTest(Type, testInstance)
 
 		// Make File
 		name := "testtoref.txt"
@@ -101,28 +120,19 @@ func TestReferences(t *testing.T) {
 
 		f, err := testInstance.VFS().CreateFile(filedoc, nil)
 		require.NoError(t, err)
-
-		if err = f.Close(); !assert.NoError(t, err) {
-			return
-		}
+		require.NoError(t, f.Close())
 
 		// update it
-		in := jsonReader(jsonapi.Relationship{
-			Data: []couchdb.DocReference{
-				{
-					ID:   filedoc.ID(),
-					Type: filedoc.DocType(),
-				},
-			},
-		})
-		req, _ := http.NewRequest("POST", url, in)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Set("Content-Type", "application/vnd.api+json")
-
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		defer res.Body.Close()
-		assert.Equal(t, 204, res.StatusCode)
+		e.POST("/data/"+doc.DocType()+"/"+doc.ID()+"/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			WithHeader("Content-Type", "application/vnd.api+json").
+			WithBytes([]byte(`{
+        "data": {
+          "id": "` + filedoc.ID() + `",
+          "type": "` + filedoc.DocType() + `"
+        }
+      }`)).
+			Expect().Status(204)
 
 		fdoc, err := testInstance.VFS().FileByID(filedoc.ID())
 		assert.NoError(t, err)
@@ -130,31 +140,28 @@ func TestReferences(t *testing.T) {
 	})
 
 	t.Run("RemoveReferencesHandler", func(t *testing.T) {
+		e := testutils.CreateTestClient(t, ts.URL)
+
 		// Make doc
-		doc := getDocForTest()
-		url := ts.URL + "/data/" + doc.DocType() + "/" + doc.ID() + "/relationships/references"
+		doc := getDocForTest(Type, testInstance)
 
 		// Make Files
-		f6 := makeReferencedTestFile(t, doc, "testtoref6.txt")
-		f7 := makeReferencedTestFile(t, doc, "testtoref7.txt")
-		f8 := makeReferencedTestFile(t, doc, "testtoref8.txt")
-		f9 := makeReferencedTestFile(t, doc, "testtoref9.txt")
+		f6 := makeReferencedTestFile(t, testInstance, doc, "testtoref6.txt")
+		f7 := makeReferencedTestFile(t, testInstance, doc, "testtoref7.txt")
+		f8 := makeReferencedTestFile(t, testInstance, doc, "testtoref8.txt")
+		f9 := makeReferencedTestFile(t, testInstance, doc, "testtoref9.txt")
 
 		// update it
-		in := jsonReader(jsonapi.Relationship{
-			Data: []couchdb.DocReference{
-				{ID: f8, Type: consts.Files},
-				{ID: f6, Type: consts.Files},
-			},
-		})
-		req, _ := http.NewRequest("DELETE", url, in)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Set("Content-Type", "application/vnd.api+json")
-
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		defer res.Body.Close()
-		assert.Equal(t, 204, res.StatusCode)
+		e.DELETE("/data/"+doc.DocType()+"/"+doc.ID()+"/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			WithHeader("Content-Type", "application/vnd.api+json").
+			WithBytes([]byte(`{
+        "data": [
+          {"id": "` + f8 + `", "type": "` + consts.Files + `"},
+          {"id": "` + f6 + `", "type": "` + consts.Files + `"}
+        ]
+      }`)).
+			Expect().Status(204)
 
 		fdoc6, err := testInstance.VFS().FileByID(f6)
 		assert.NoError(t, err)
@@ -172,6 +179,8 @@ func TestReferences(t *testing.T) {
 	})
 
 	t.Run("ReferencesWithSlash", func(t *testing.T) {
+		e := testutils.CreateTestClient(t, ts.URL)
+
 		// Make File
 		name := "test-ref-with-slash.txt"
 		dirID := consts.RootDirID
@@ -181,28 +190,18 @@ func TestReferences(t *testing.T) {
 
 		f, err := testInstance.VFS().CreateFile(filedoc, nil)
 		require.NoError(t, err)
-
-		if err = f.Close(); !assert.NoError(t, err) {
-			return
-		}
+		require.NoError(t, f.Close())
 
 		// Add a reference to io.cozy.apps/foobar
-		url := ts.URL + "/data/" + Type + "/io.cozy.apps%2ffoobar/relationships/references"
-		in := jsonReader(jsonapi.Relationship{
-			Data: []couchdb.DocReference{
-				{
-					ID:   filedoc.ID(),
-					Type: filedoc.DocType(),
-				},
-			},
-		})
-		req, _ := http.NewRequest("POST", url, in)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Set("Content-Type", "application/vnd.api+json")
-		res, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		defer res.Body.Close()
-		assert.Equal(t, 204, res.StatusCode)
+		e.POST("/data/"+Type+"/io.cozy.apps%2ffoobar/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			WithHeader("Content-Type", "application/vnd.api+json").
+			WithBytes([]byte(`{
+        "data": [
+          {"id": "` + filedoc.ID() + `", "type": "` + filedoc.DocType() + `"}
+        ]
+      }`)).
+			Expect().Status(204)
 
 		fdoc, err := testInstance.VFS().FileByID(filedoc.ID())
 		assert.NoError(t, err)
@@ -210,45 +209,37 @@ func TestReferences(t *testing.T) {
 		assert.Equal(t, "io.cozy.apps/foobar", fdoc.ReferencedBy[0].ID)
 
 		// Check that we can find the reference with /
-		var result struct {
-			Data []couchdb.DocReference `json:"data"`
-			Meta jsonapi.Meta
-		}
-		req, _ = http.NewRequest("GET", url, nil)
-		req.Header.Add("Authorization", "Bearer "+token)
-		_, res, err = doRequest(req, &result)
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res.StatusCode)
-		assert.Len(t, result.Data, 1)
-		assert.Equal(t, *result.Meta.Count, 1)
-		assert.Equal(t, fdoc.ID(), result.Data[0].ID)
+		obj := e.GET("/data/"+Type+"/io.cozy.apps%2ffoobar/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
+
+		obj.Path("$.meta.count").Equal(1)
+		obj.Value("data").Array().Length().Equal(1)
+		obj.Path("$.data[0].id").Equal(fdoc.ID())
 
 		// Try again, but this time encode / as %2F instead of %2f
-		url = ts.URL + "/data/" + Type + "/io.cozy.apps%2Ffoobar/relationships/references"
-		result.Data = result.Data[:0]
-		result.Meta = jsonapi.Meta{}
-		req, _ = http.NewRequest("GET", url, nil)
-		req.Header.Add("Authorization", "Bearer "+token)
-		_, res, err = doRequest(req, &result)
-		assert.NoError(t, err)
-		assert.Equal(t, 200, res.StatusCode)
-		assert.Len(t, result.Data, 1)
-		assert.Equal(t, *result.Meta.Count, 1)
-		assert.Equal(t, fdoc.ID(), result.Data[0].ID)
+		obj = e.GET("/data/"+Type+"/io.cozy.apps%2Ffoobar/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(200).
+			JSON(httpexpect.ContentOpts{MediaType: "application/vnd.api+json"}).
+			Object()
+
+		obj.Path("$.meta.count").Equal(1)
+		obj.Value("data").Array().Length().Equal(1)
+		obj.Path("$.data[0].id").Equal(fdoc.ID())
 
 		// Remove the reference with a /
-		in = jsonReader(jsonapi.Relationship{
-			Data: []couchdb.DocReference{
-				{ID: fdoc.ID(), Type: consts.Files},
-			},
-		})
-		req, _ = http.NewRequest("DELETE", url, in)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Set("Content-Type", "application/vnd.api+json")
-		res, err = http.DefaultClient.Do(req)
-		assert.NoError(t, err)
-		defer res.Body.Close()
-		assert.Equal(t, 204, res.StatusCode)
+		e.DELETE("/data/"+Type+"/io.cozy.apps%2Ffoobar/relationships/references").
+			WithHeader("Authorization", "Bearer "+token).
+			WithHeader("Content-Type", "application/vnd.api+json").
+			WithBytes([]byte(`{
+        "data": [
+          {"id": "` + fdoc.ID() + `", "type": "` + consts.Files + `"}
+        ]
+      }`)).
+			Expect().Status(204)
 
 		// Check that all the references have been removed
 		fdoc2, err := testInstance.VFS().FileByID(fdoc.ID())
@@ -257,7 +248,7 @@ func TestReferences(t *testing.T) {
 	})
 }
 
-func makeReferencedTestFile(t *testing.T, doc couchdb.Doc, name string) string {
+func makeReferencedTestFile(t *testing.T, instance *instance.Instance, doc couchdb.Doc, name string) string {
 	dirID := consts.RootDirID
 	mime, class := vfs.ExtractMimeAndClassFromFilename(name)
 	filedoc, err := vfs.NewFileDoc(name, dirID, -1, nil, mime, class, time.Now(), false, false, false, nil)
@@ -272,7 +263,7 @@ func makeReferencedTestFile(t *testing.T, doc couchdb.Doc, name string) string {
 		},
 	}
 
-	f, err := testInstance.VFS().CreateFile(filedoc, nil)
+	f, err := instance.VFS().CreateFile(filedoc, nil)
 	if !assert.NoError(t, err) {
 		return ""
 	}
