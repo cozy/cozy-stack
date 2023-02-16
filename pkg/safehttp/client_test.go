a95dec55c313615a5124de68e7a2e465ca7adf67
Peltoche <pierre.peltier@protonmail.com>
2023-02-16 14:42:59 +0100

    Add some `//nolint` comment in `pkg/safehttp` in order to avoid some `bodyclose` errors

--- pkg/safehttp/client_test.go
+++ pkg/safehttp/client_test.go
@@ -16,10 +16,12 @@ func TestDefaultClient(t *testing.T) {
 	require.NoError(t, err)
 	defer res.Body.Close()
 
+	//nolint: bodyclose // This request fail so there is no body to close
 	_, err = DefaultClient.Get("http://192.168.0.1/")
 	require.Error(t, err)
 	assert.Contains(t, err.Error(), "is not a public IP address")
 
+	//nolint: bodyclose // This request fail so there is no body to close
 	_, err = DefaultClient.Get("http://1.2.3.4:5984/")
 	require.Error(t, err)
 	assert.Contains(t, err.Error(), "is not a safe port")
