a9d77bfd8d8442655b13877b583b0e731a538a69
Peltoche <pierre.peltier@protonmail.com>
2023-03-06 16:06:04 +0100

    Move the vault logic into a new `keyring` package
    Previously the vault logic was living inside the `/pkg/config` package
    in a global alongs with other config stuff.
    
    This PR goal is to make a clear separation between the vault logic,
    where some important secrets are handled and the config logic. It also
    remove the global variable and move to the same initialization logic
    than the cache or lock package: It create an object and put it into the
    `Config` object.

---
+++ pkg/keyring/service.go
@@ -0,0 +1,89 @@
+package keyring
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/cozy/cozy-stack/pkg/keymgmt"
+)
+
+var (
+	ErrFieldRequired = errors.New("field required")
+)
+
+// Keyring handle the encryption/decryption keys
+type Keyring interface {
+	// CredentialsEncryptorKey returns the key used to encrypt credentials values,
+	// stored in accounts.
+	CredentialsEncryptorKey() *keymgmt.NACLKey
+	// CredentialsDecryptorKey returns the key used to decrypt credentials values,
+	// stored in accounts.
+	CredentialsDecryptorKey() *keymgmt.NACLKey
+}
+
+// Config used to setup a [Keyring] service.
+type Config struct {
+	EncryptorKeyPath string `mapstructure:"credentials_encryptor_key"`
+	DecryptorKeyPath string `mapstructure:"credentials_decryptor_key"`
+}
+
+// Service contains security keys used for various encryption or signing of
+// critical assets.
+type Service struct {
+	credsEncryptor *keymgmt.NACLKey
+	credsDecryptor *keymgmt.NACLKey
+}
+
+func NewFromConfig(conf Config) (Keyring, error) {
+	if conf.DecryptorKeyPath == "" || conf.EncryptorKeyPath == "" {
+		return NewStub()
+	}
+
+	return NewService(conf)
+}
+
+// NewService instantiate a new [Keyring].
+func NewService(conf Config) (*Service, error) {
+	if conf.EncryptorKeyPath == "" {
+		return nil, fmt.Errorf("credentials_encryptor_key: %w", ErrFieldRequired)
+	}
+
+	if conf.DecryptorKeyPath == "" {
+		return nil, fmt.Errorf("credentials_decryptor_key: %w", ErrFieldRequired)
+	}
+
+	credsEncryptor, err := decodeKeyFromPath(conf.EncryptorKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	credsDecryptor, err := decodeKeyFromPath(conf.DecryptorKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Service{credsEncryptor, credsDecryptor}, nil
+}
+
+func (s *Service) CredentialsEncryptorKey() *keymgmt.NACLKey {
+	return s.credsEncryptor
+}
+
+func (s *Service) CredentialsDecryptorKey() *keymgmt.NACLKey {
+	return s.credsDecryptor
+}
+
+func decodeKeyFromPath(path string) (*keymgmt.NACLKey, error) {
+	keyBytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %q: %w", path, err)
+	}
+
+	creds, err := keymgmt.UnmarshalNACLKey(keyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal NACL key: %w", err)
+	}
+
+	return creds, nil
+}
