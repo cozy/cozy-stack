9ef2aa61de25ca7f68287d7e61fc48eb266c3d36
Peltoche <pierre.peltier@protonmail.com>
2023-05-15 21:23:12 +0200

    test: Test the method `ServeContent`

--- pkg/utils/serve_content_test.go
+++ pkg/utils/serve_content_test.go
@@ -3,10 +3,43 @@ package utils
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_ServeContent(t *testing.T) {
+	t.Run("with other than a HEAD method", func(t *testing.T) {
+		r := httptest.NewRequest(http.MethodGet, "http://localhost/foo/bar", nil)
+		w := httptest.NewRecorder()
+
+		ServeContent(w, r, "application/json", 10, strings.NewReader("foobar"))
+
+		assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+		assert.Equal(t, "application/json", w.Result().Header.Get("Content-Type"))
+		assert.Equal(t, "foobar", w.Body.String())
+
+		// Not that the length 10 as the `size` parameter and not `len("foobar")`
+		assert.Equal(t, "10", w.Result().Header.Get("Content-Length"))
+	})
+
+	t.Run("with the HEAD method", func(t *testing.T) {
+		r := httptest.NewRequest(http.MethodHead, "http://localhost/foo/bar", nil)
+		w := httptest.NewRecorder()
+
+		ServeContent(w, r, "application/json", 10, strings.NewReader("foobar"))
+
+		assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+		assert.Equal(t, "application/json", w.Result().Header.Get("Content-Type"))
+
+		// With HEAD we don't setup a body
+		assert.Empty(t, w.Body.String())
+
+		// Not that the length 10 as the `size` parameter and not `len("foobar")`
+		assert.Equal(t, "10", w.Result().Header.Get("Content-Length"))
+	})
+}
 
 func Test_CheckPreconditions_with_matching_etag(t *testing.T) {
 	r := httptest.NewRequest(http.MethodGet, "http://localhost/foo/bar", nil)
