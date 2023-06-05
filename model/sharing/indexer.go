ae0a572d79c8d1cb4049fa0f5cb911351c7213ee
Bruno Michel <bmichel@menfin.info>
2023-06-05 17:49:08 +0200

    Allow to add metadata on directory creation

--- model/sharing/indexer.go
+++ model/sharing/indexer.go
@@ -312,6 +312,9 @@ func (s *sharingIndexer) UpdateDirDoc(olddoc, doc *vfs.DirDoc) error {
 	if len(doc.ReferencedBy) > 0 {
 		docs[0][couchdb.SelectorReferencedBy] = doc.ReferencedBy
 	}
+	if doc.Metadata != nil {
+		docs[0]["metadata"] = doc.Metadata
+	}
 	if doc.CozyMetadata != nil {
 		docs[0]["cozyMetadata"] = doc.CozyMetadata
 	}
