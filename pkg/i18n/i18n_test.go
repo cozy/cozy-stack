2cebe823fb924d81376f0712d977652e3ad0c17d
Erwan Guyader <erwan.guyader@cozycloud.cc>
2023-12-21 11:59:22 +0100

    feat: Transform newlines in HTML translations
      Some localization strings include new line characters (i.e. `\n`) to
      control the flow of text while avoiding the multiplication of strings
      to manage (i.e. one per line).
    
      However, these characters were not transformed into `<br />` tags by
      the HTML translator thus effectively losing the expected text flow in
      the resulting HTML.

--- pkg/i18n/i18n_test.go
+++ pkg/i18n/i18n_test.go
@@ -1,6 +1,7 @@
 package i18n
 
 import (
+	"html/template"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,4 +46,52 @@ msgstr "contexte foo"
 	assert.Equal(t, "contexte foo", s)
 	s = Translate("context", "fr", "bar")
 	assert.Equal(t, "contexte", s)
+}
+
+func TestTranslatorHTML(t *testing.T) {
+	contextName := "foo"
+
+	LoadLocale("fr", "", []byte(`
+msgid "english"
+msgstr "french"
+
+msgid "hello %s"
+msgstr "bonjour **%s**"
+
+msgid "context"
+msgstr "contexte"
+`))
+
+	LoadLocale("fr", contextName, []byte(`
+msgid "english"
+msgstr "french"
+
+msgid "hello %s"
+msgstr "bonjour **%s**"
+
+msgid "context"
+msgstr ""
+"contexte\n"
+"foo"
+`))
+
+	frHTML := TranslatorHTML("fr", contextName)
+
+	h := frHTML("english")
+	assert.Equal(t, template.HTML("french"), h)
+	h = frHTML("hello %s", "toto")
+	assert.Equal(t, template.HTML("bonjour <strong>toto</strong>"), h)
+
+	enHTML := TranslatorHTML("en", contextName)
+	h = enHTML("english")
+	assert.Equal(t, template.HTML("english"), h)
+	h = enHTML("hello %s", "toto")
+	assert.Equal(t, template.HTML("hello toto"), h)
+
+	h = frHTML("context")
+	assert.Equal(t, template.HTML("contexte<br />foo"), h)
+
+	barHTML := TranslatorHTML("fr", "bar")
+	h = barHTML("context")
+	assert.Equal(t, template.HTML("contexte"), h)
 }
