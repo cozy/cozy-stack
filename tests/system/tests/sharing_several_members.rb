d5529b1d30a19fc7f9f4c9e12bba0d828da74804
Bruno Michel <bmichel@menfin.info>
2024-01-11 15:26:22 +0100

    Fix a sharing bug, where a deleted file can reappear
    Expected scenario:
    
    1. There is a sharing, owned by Alice, with several members
    2. Bob, of the recipient, adds a file in the shared directory
    3. About 20-25 seconds later, Bob puts the file in the trash
    4. The file should be trashed on all the instances of the sharing
    
    What happened:
    
    - The file is replicated to Alice's instance
    - 10 seconds later, the file is replicated to the other instances
    - The file is trashed on Alice's instance
    - One of the other instance (let's say Charlie's instance) runs its
      share_upload worker, which recreates the file on Alice's instance.
    
    The bug is to not ignore the "echo" of file creation. We can do that by
    checking the shared doc and see that the revision is already known.

---
+++ tests/system/tests/sharing_several_members.rb
@@ -0,0 +1,78 @@
+require_relative '../boot'
+require 'minitest/autorun'
+require 'pry-rescue/minitest' unless ENV['CI']
+
+describe "A sharing with several members" do
+  it "is correctly synchronized" do
+    Helpers.scenario "several_members"
+    Helpers.start_mailhog
+
+    # Create the instance
+    inst = Instance.create name: "Alice"
+    inst_bob = Instance.create name: "Bob"
+    inst_charlie = Instance.create name: "Charlie"
+    inst_dave = Instance.create name: "Dave"
+    inst_emily = Instance.create name: "Emily"
+
+    # Create the hierarchy
+    folder = Folder.create inst
+    folder.couch_id.wont_be_empty
+    content_path = "../fixtures/wet-cozy_20160910__M4Dz.jpg"
+    opts = CozyFile.options_from_fixture(content_path, dir_id: folder.couch_id)
+    f1 = CozyFile.create inst, opts
+
+    # Create the sharing
+    contact_bob = Contact.create inst, given_name: "Bob"
+    contact_charlie = Contact.create inst, given_name: "Charlie"
+    contact_dave = Contact.create inst, given_name: "Dave"
+    contact_emily = Contact.create inst, given_name: "Emily"
+    sharing = Sharing.new
+    sharing.rules << Rule.sync(folder)
+    sharing.members << inst << contact_bob << contact_charlie << contact_dave << contact_emily
+    inst.register sharing
+
+    # Accept the sharing
+    sleep 1
+    inst_bob.accept sharing
+    inst_charlie.accept sharing
+    inst_dave.accept sharing
+    inst_emily.accept sharing
+    sleep 12
+
+    dir_path = "/#{Helpers::SHARED_WITH_ME}/#{folder.name}"
+    dir_bob = Folder.find_by_path inst_bob, dir_path
+    opts = CozyFile.options_from_fixture(content_path, dir_id: dir_bob.couch_id)
+    f2_bob = CozyFile.create inst_bob, opts
+    sleep 23
+
+    f2_bob.remove inst_bob
+    sleep 21
+
+    # Check that the files are the same on disk
+    da = File.join Helpers.current_dir, inst.domain, folder.name
+    db = File.join Helpers.current_dir, inst_bob.domain,
+                   Helpers::SHARED_WITH_ME, sharing.rules.first.title
+    dc = File.join Helpers.current_dir, inst_charlie.domain,
+                   Helpers::SHARED_WITH_ME, sharing.rules.first.title
+    dd = File.join Helpers.current_dir, inst_dave.domain,
+                   Helpers::SHARED_WITH_ME, sharing.rules.first.title
+    de = File.join Helpers.current_dir, inst_emily.domain,
+                   Helpers::SHARED_WITH_ME, sharing.rules.first.title
+    Helpers.fsdiff(da, db).must_be_empty
+    Helpers.fsdiff(da, dc).must_be_empty
+    Helpers.fsdiff(da, dd).must_be_empty
+    Helpers.fsdiff(da, de).must_be_empty
+
+    assert_equal inst.check, []
+    assert_equal inst_bob.check, []
+    assert_equal inst_charlie.check, []
+    assert_equal inst_dave.check, []
+    assert_equal inst_emily.check, []
+
+    inst.remove
+    inst_bob.remove
+    inst_charlie.remove
+    inst_dave.remove
+    inst_emily.remove
+  end
+end
