cozy/cozy-stack

commit 0a22f21920f7f37691fd70104e9fb232489f83e9

Fix a sharing bug with sharing dir not created
Scenario:

1. The URL of Bob's Cozy is known by Alice's Cozy from a previous
   sharing
2. Alice creates a directory Foo and puts a file Bar inside it
3. Alice sends a sharing invitation for this directory to Bob
4. Alice deletes the Bar file making Foo empty
5. Bob accepts to synchronize the sharing on their Cozy

Expected: the shortcut for the sharing on Bob's Cozy is replaced by an
empty Foo directory
Actual: it doesn't happen because initial_number_of_files_to_sync is
computed when the sharing is created, and no file is shared.

This fix ensures that the sharing directory exists when the initial
synchronization is done (by creating it if needed).

model/sharing/sharing.go
@@ -800,6 +800,11 @@ func (s *Sharing) EndInitial(inst *instance.Instance) error {
 		M:    map[string]interface{}{"_id": s.SID},
 	}
 	realtime.GetHub().Publish(inst, realtime.EventDelete, &doc, nil)
+	if s.FirstFilesRule() != nil {
+		if _, err := s.GetSharingDir(inst); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 

commit 804f584c35631b9774e9287e4f45f9644eb0ca2d

Fix a regression when sharing a single file
The regression was introduced by
https://github.com/cozy/cozy-stack/commit/0a22f21920f7f37691fd70104e9fb232489f83e9

When a single file is shared, the call to notify the end of the initial
replication tries to create a sharing directory, which creates a CouchDB
conflict. The fix is just checking that case to avoid creating the
directory.

model/sharing/files.go
@@ -339,6 +339,11 @@ func (s *Sharing) GetSharingDir(inst *instance.Instance) (*vfs.DirDoc, error) {
 	fs := inst.VFS()
 	rule := s.FirstFilesRule()
 	if rule != nil {
+		if rule.Mime != "" {
+			inst.Logger().WithNamespace("sharing").
+				Warnf("GetSharingDir called for only one file: %s", s.SID)
+			return nil, ErrInternalServerError
+		}
 		dir, _ := fs.DirByID(rule.Values[0])
 		if dir != nil {
 			return dir, nil

model/sharing/sharing.go
@@ -800,7 +800,7 @@ func (s *Sharing) EndInitial(inst *instance.Instance) error {
 		M:    map[string]interface{}{"_id": s.SID},
 	}
 	realtime.GetHub().Publish(inst, realtime.EventDelete, &doc, nil)
-	if s.FirstFilesRule() != nil {
+	if rule := s.FirstFilesRule(); rule != nil && rule.Mime == "" {
 		if _, err := s.GetSharingDir(inst); err != nil {
 			return err
 		}