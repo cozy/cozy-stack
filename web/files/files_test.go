9af163dbb7f0db712ef92904af0f7c1043e980eb
Bruno Michel <bmichel@menfin.info>
2024-03-25 12:29:52 +0100

    Fix build

--- web/files/files_test.go
+++ web/files/files_test.go
@@ -3616,7 +3616,7 @@ func TestFiles(t *testing.T) {
 	})
 
 	t.Run("DeprecatePreviewAndIcon", func(t *testing.T) {
-		testutils.TODO(t, "2024-03-01", "Remove the deprecated preview and icon for PDF files")
+		testutils.TODO(t, "2024-05-01", "Remove the deprecated preview and icon for PDF files")
 	})
 }
 
