106f8db57fcf85e4a36a7c1f520de5470e3f188a
Bruno Michel <bruno@cozycloud.cc>
2025-09-15 17:38:35 +0200

    fix: Fix opening notes and office docs in shared drives

--- web/notes/notes.go
+++ web/notes/notes.go
@@ -338,7 +338,9 @@ func OpenNoteURL(c echo.Context) error {
 	// If a directory is shared by link and contains a note, the note can be
 	// opened with the same sharecode as the directory. The sharecode is also
 	// used to identify the member that previews a sharing.
-	if pdoc.Type == permission.TypeShareByLink || pdoc.Type == permission.TypeSharePreview {
+	if pdoc.Type == permission.TypeShareByLink ||
+		pdoc.Type == permission.TypeSharePreview ||
+		pdoc.Type == permission.TypeShareInteract {
 		code := middlewares.GetRequestToken(c)
 		open.AddShareByLinkCode(code)
 	}
