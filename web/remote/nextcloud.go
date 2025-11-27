868e1ffbc04561f738e13029c7b8b4d5929942ba
Crash-- <quentin.valmori@gmail.com>
2025-11-27 16:46:09 +0100

    feat: 409 on conflict for nextcloud

--- web/remote/nextcloud.go
+++ web/remote/nextcloud.go
@@ -264,8 +264,10 @@ func nextcloudDownstream(c echo.Context) error {
 		kind = nextcloud.CopyOperation
 	}
 
+	failOnConflict, _ := strconv.ParseBool(c.QueryParam("FailOnConflict"))
+
 	cozyMetadata, _ := files.CozyMetadataFromClaims(c, true)
-	f, err := nc.Downstream(path, to, kind, cozyMetadata)
+	f, err := nc.Downstream(path, to, kind, cozyMetadata, failOnConflict)
 	if err != nil {
 		return wrapNextcloudErrors(err)
 	}
