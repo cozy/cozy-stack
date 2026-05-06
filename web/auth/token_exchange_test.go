73ceed46fdd840586a03347e236d6a851d8da848
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-05-06 16:06:34 +0200

    feat: Add io.cozy.apps in token echange
    Used in cozy-sharing provider

--- web/auth/token_exchange_test.go
+++ web/auth/token_exchange_test.go
@@ -9,7 +9,7 @@ import (
 func TestValidateTokenExchangeScope(t *testing.T) {
 	assert.Error(t, validateTokenExchangeScope(""))
 	assert.Error(t, validateTokenExchangeScope("io.cozy.unknown"))
-	assert.Error(t, validateTokenExchangeScope("io.cozy.files\tio.cozy.contacts\tio.cozy.contacts.groups"))
+	assert.Error(t, validateTokenExchangeScope("io.cozy.files\tio.cozy.contacts\tio.cozy.contacts.groups\tio.cozy.apps"))
 	assert.NoError(t, validateTokenExchangeScope("io.cozy.files"))
-	assert.NoError(t, validateTokenExchangeScope("io.cozy.files io.cozy.contacts io.cozy.contacts.groups"))
+	assert.NoError(t, validateTokenExchangeScope("io.cozy.files io.cozy.contacts io.cozy.contacts.groups io.cozy.apps"))
 }
